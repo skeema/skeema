9c00941fb98f82afa9ec8a64146dc954d7975c9f
Evan Elias <evan@skeema.net>
2018-08-24 01:21:58 -0400

    IsDatabaseError(): Make optionally variadic to select particular errors only

--- errors.go
+++ errors.go
@@ -7,17 +7,24 @@ import (
 
 // IsDatabaseError returns true if err came from a database server, typically
 // as a response to a query or connection attempt.
-func IsDatabaseError(err error) bool {
-	_, result := err.(*mysql.MySQLError)
-	return result
+// If one or more specificErrors are supplied, IsDatabaseError only returns true
+// if the database error code matched one of those numbers.
+func IsDatabaseError(err error, specificErrors ...uint16) bool {
+	merr, ok := err.(*mysql.MySQLError)
+	if !ok || len(specificErrors) == 0 {
+		return ok
+	}
+	for _, num := range specificErrors {
+		if merr.Number == num {
+			return true
+		}
+	}
+	return false
 }
 
 // IsSyntaxError returns true if err is a SQL syntax error, or false otherwise.
 func IsSyntaxError(err error) bool {
-	if merr, ok := err.(*mysql.MySQLError); ok {
-		return merr.Number == mysqlerr.ER_PARSE_ERROR || merr.Number == mysqlerr.ER_SYNTAX_ERROR
-	}
-	return false
+	return IsDatabaseError(err, mysqlerr.ER_PARSE_ERROR, mysqlerr.ER_SYNTAX_ERROR)
 }
 
 // IsAccessError returns true if err indicates an authentication or authorization
@@ -26,17 +33,14 @@ func IsSyntaxError(err error) bool {
 // There is no sense in immediately retrying the connection or query when
 // encountering this type of error.
 func IsAccessError(err error) bool {
-	if merr, ok := err.(*mysql.MySQLError); ok {
-		authErrors := map[uint16]bool{
-			mysqlerr.ER_ACCESS_DENIED_ERROR:          true,
-			mysqlerr.ER_BAD_HOST_ERROR:               true,
-			mysqlerr.ER_DBACCESS_DENIED_ERROR:        true,
-			mysqlerr.ER_BAD_DB_ERROR:                 true,
-			mysqlerr.ER_HOST_NOT_PRIVILEGED:          true,
-			mysqlerr.ER_HOST_IS_BLOCKED:              true,
-			mysqlerr.ER_SPECIFIC_ACCESS_DENIED_ERROR: true,
-		}
-		return authErrors[merr.Number]
+	authErrors := []uint16{
+		mysqlerr.ER_ACCESS_DENIED_ERROR,
+		mysqlerr.ER_BAD_HOST_ERROR,
+		mysqlerr.ER_DBACCESS_DENIED_ERROR,
+		mysqlerr.ER_BAD_DB_ERROR,
+		mysqlerr.ER_HOST_NOT_PRIVILEGED,
+		mysqlerr.ER_HOST_IS_BLOCKED,
+		mysqlerr.ER_SPECIFIC_ACCESS_DENIED_ERROR,
 	}
-	return false
+	return IsDatabaseError(err, authErrors...)
 }
