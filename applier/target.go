skeema/skeema

commit 9f8c88a7cb041630ff45b3268d556fa87f910a06

Internal refactoring to improve gocyclo score
This commit refactors a number of overly-large functions, in order to reduce
cyclomatic complexity. No change in behavior or functionality is made.

applier/target.go
@@ -38,6 +38,59 @@ func (t *Target) SchemaFromDir() *tengo.Schema {
 	return &schemaCopy
 }
 
+// dryRun returns true if this target is only being used for dry-run purposes,
+// rather than actually wanting to apply changes to this target.
+func (t *Target) dryRun() bool {
+	return t.Dir.Config.GetBool("dry-run")
+}
+
+// briefOutput returns true if this target is only being evaluated for having
+// differences or not.
+func (t *Target) briefOutput() bool {
+	return t.Dir.Config.GetBool("brief") && t.dryRun()
+}
+
+func (t *Target) logApplyStart() {
+	if t.dryRun() {
+		log.Infof("Generating diff of %s %s vs %s/*.sql", t.Instance, t.SchemaName, t.Dir)
+	} else {
+		log.Infof("Pushing changes from %s/*.sql to %s %s", t.Dir, t.Instance, t.SchemaName)
+	}
+	if len(t.Dir.IgnoredStatements) > 0 {
+		log.Warnf("Ignoring %d unsupported or unparseable statements found in this directory's *.sql files; run `skeema lint` for more info", len(t.Dir.IgnoredStatements))
+	}
+}
+
+func (t *Target) logApplyEnd(result Result) {
+	if result.Differences {
+		log.Infof("%s %s: No differences found\n", t.Instance, t.SchemaName)
+	} else {
+		verb := "push"
+		if t.dryRun() {
+			verb = "diff"
+		}
+		log.Infof("%s %s: %s complete\n", t.Instance, t.SchemaName, verb)
+	}
+}
+
+func (t *Target) processDDL(ddls []*DDLStatement, printer *Printer) (skipCount int) {
+	for i, ddl := range ddls {
+		printer.printDDL(ddl)
+		if !t.dryRun() {
+			if err := ddl.Execute(); err != nil {
+				log.Errorf("Error running DDL on %s %s: %s", t.Instance, t.SchemaName, err)
+				skipped := len(ddls) - i
+				skipCount += skipped
+				if skipped > 1 {
+					log.Warnf("Skipping %d remaining operations for %s %s due to previous error", skipped-1, t.Instance, t.SchemaName)
+				}
+				return
+			}
+		}
+	}
+	return
+}
+
 // TargetGroup represents a group of Targets that all have the same Instance.
 type TargetGroup []*Target
 
@@ -61,10 +114,12 @@ func TargetsForDir(dir *fs.Dir, maxDepth int) (targets []*Target, skipCount int)
 
 		// For each LogicalSchema, obtain a *tengo.Schema representation and then
 		// create a Target for each instance x schema combination
-		for _, logicalSchema := range dir.LogicalSchemas {
-			thisTargets, thisSkipCount := targetsForLogicalSchema(logicalSchema, dir, instances)
-			targets = append(targets, thisTargets...)
-			skipCount += thisSkipCount
+		if len(instances) > 0 {
+			for _, logicalSchema := range dir.LogicalSchemas {
+				thisTargets, thisSkipCount := targetsForLogicalSchema(logicalSchema, dir, instances)
+				targets = append(targets, thisTargets...)
+				skipCount += thisSkipCount
+			}
 		}
 	} else if dir.HasSchema() {
 		// If we have a schema defined but no host, display a warning
@@ -154,11 +209,6 @@ func instancesForDir(dir *fs.Dir) (instances []*tengo.Instance, skipCount int) {
 }
 
 func targetsForLogicalSchema(logicalSchema *fs.LogicalSchema, dir *fs.Dir, instances []*tengo.Instance) (targets []*Target, skipCount int) {
-	// If dir mapped to no instances, it generates no targets
-	if len(instances) == 0 {
-		return
-	}
-
 	// Obtain a *tengo.Schema representation of the dir's *.sql files from a
 	// workspace
 	opts, err := workspace.OptionsForDir(dir, instances[0])
@@ -173,7 +223,7 @@ func targetsForLogicalSchema(logicalSchema *fs.LogicalSchema, dir *fs.Dir, insta
 	}
 	for _, stmtErr := range wsSchema.Failures {
 		log.Error(stmtErr.Error())
-		if (strings.Contains(stmtErr.Error(), "Error 1031") || strings.Contains(stmtErr.Error(), "Error 1067")) && !dir.Config.Changed("connect-options") {
+		if isStrictModeError(stmtErr) && !dir.Config.Changed("connect-options") {
 			log.Info("This may be caused by Skeema's default usage of strict-mode settings. To disable strict-mode, add this to a .skeema file:")
 			log.Info("connect-options=\"innodb_strict_mode=0,sql_mode='ONLY_FULL_GROUP_BY,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'\"\n")
 		}
@@ -234,4 +284,9 @@ func TargetGroupChanForDir(dir *fs.Dir) (<-chan TargetGroup, int) {
 		close(groups)
 	}()
 	return groups, skipCount
+}
+
+func isStrictModeError(err error) bool {
+	message := err.Error()
+	return strings.Contains(message, "Error 1031") || strings.Contains(message, "Error 1067")
 }

applier/applier.go
@@ -4,6 +4,7 @@ package applier
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -19,129 +20,38 @@ type Result struct {
 	UnsupportedCount int
 }
 
+// Summary returns a string reflecting the contents of the result.
+func (r Result) Summary() string {
+	if r.SkipCount+r.UnsupportedCount == 0 {
+		return ""
+	}
+	var plural, reason string
+	if r.SkipCount+r.UnsupportedCount > 1 {
+		plural = "s"
+	}
+	if r.SkipCount == 0 {
+		reason = "unsupported feature"
+	} else if r.UnsupportedCount == 0 {
+		reason = "error"
+	} else {
+		reason = "unsupported features or error"
+	}
+	return fmt.Sprintf("Skipped %d operation%s due to %s%s", r.SkipCount+r.UnsupportedCount, plural, reason, plural)
+}
+
 // Worker reads TargetGroups from the input channel and performs the appropriate
 // diff/push operation on each target per TargetGroup. When there are no more
 // TargetGroups to read, it writes its aggregate Result to the output channel.
 // If a fatal error occurs, it will be returned immediately; Worker is meant to
 // be called via an errgroup (see golang.org/x/sync/errgroup).
 func Worker(ctx context.Context, targetGroups <-chan TargetGroup, results chan<- Result, printer *Printer) error {
-	var result Result
 	for tg := range targetGroups {
-	TargetsInGroup:
-		for _, t := range tg { // iterate over each Target in the TargetGroup
-			dryRun := t.Dir.Config.GetBool("dry-run")
-			brief := dryRun && t.Dir.Config.GetBool("brief")
-
-			if dryRun {
-				log.Infof("Generating diff of %s %s vs %s/*.sql", t.Instance, t.SchemaName, t.Dir)
-			} else {
-				log.Infof("Pushing changes from %s/*.sql to %s %s", t.Dir, t.Instance, t.SchemaName)
-			}
-			if len(t.Dir.IgnoredStatements) > 0 {
-				log.Warnf("Ignoring %d unsupported or unparseable statements found in this directory's *.sql files; run `skeema lint` for more info", len(t.Dir.IgnoredStatements))
-			}
-
-			schemaFromInstance, err := t.SchemaFromInstance()
+		for _, t := range tg {
+			result, err := applyTarget(t, printer)
 			if err != nil {
-				result.SkipCount++
-				log.Errorf("Skipping %s schema %s for %s: %s", t.Instance, t.SchemaName, t.Dir, err)
+				return err
 			}
-			diff := tengo.NewSchemaDiff(schemaFromInstance, t.SchemaFromDir())
-			var targetStmtCount int
-
-			if t.Dir.Config.GetBool("verify") && len(diff.TableDiffs) > 0 && !brief {
-				if err := VerifyDiff(diff, t); err != nil {
-					return err
-				}
-			}
-
-			// Obtain StatementModifiers based on the dir's config, along with linter
-			// options
-			mods, err := StatementModifiersForDir(t.Dir)
-			if err != nil {
-				return ConfigError(err.Error())
-			}
-			mods.Flavor = t.Instance.Flavor()
-			lintOpts, err := linter.OptionsForDir(t.Dir)
-			if err != nil {
-				return ConfigError(err.Error())
-			}
-
-			// Build DDLStatements for each ObjectDiff, handling pre-execution errors
-			// accordingly. Also track ObjectKeys for modified objects, for subsequent
-			// use in linting.
-			objDiffs := diff.ObjectDiffs()
-			ddls := make([]*DDLStatement, 0, len(objDiffs))
-			keys := make([]tengo.ObjectKey, 0, len(objDiffs))
-			for _, objDiff := range objDiffs {
-				ddl, err := NewDDLStatement(objDiff, mods, t)
-				if ddl == nil && err == nil {
-					continue // Skip entirely if mods made the statement a noop
-				}
-				targetStmtCount++
-				result.Differences = true
-				if err == nil {
-					ddls = append(ddls, ddl)
-					keys = append(keys, objDiff.ObjectKey())
-				} else if unsupportedErr, ok := err.(*tengo.UnsupportedDiffError); ok {
-					result.UnsupportedCount++
-					log.Warnf("Skipping %s: unable to generate DDL due to use of unsupported features. Use --debug for more information.", unsupportedErr.ObjectKey)
-					DebugLogUnsupportedDiff(unsupportedErr)
-				} else {
-					result.SkipCount += len(objDiffs)
-					log.Errorf(err.Error())
-					if len(objDiffs) > 1 {
-						log.Warnf("Skipping %d additional operations for %s %s due to previous error", len(objDiffs)-1, t.Instance, t.SchemaName)
-					}
-					continue TargetsInGroup
-				}
-			}
-
-			// Lint any modified objects; output the result; skip target if any
-			// annotations are at the error level
-			if t.Dir.Config.GetBool("lint") {
-				lintOpts.OnlyKeys(keys)
-				lintResult := linter.CheckSchema(t.DesiredSchema, lintOpts)
-				lintResult.SortByFile()
-				for _, annotation := range lintResult.Annotations {
-					annotation.Log()
-				}
-				if lintResult.ErrorCount > 0 {
-					noun := "error"
-					if lintResult.ErrorCount > 1 {
-						noun = "errors"
-					}
-					result.SkipCount += len(objDiffs)
-					log.Warnf("Skipping %s %s due to %d linter %s", t.Instance, t.SchemaName, lintResult.ErrorCount, noun)
-					continue TargetsInGroup
-				}
-			}
-
-			// Print DDL; if not dry-run, execute it
-			for i, ddl := range ddls {
-				printer.printDDL(ddl)
-				if !dryRun {
-					if err := ddl.Execute(); err != nil {
-						log.Errorf("Error running DDL on %s %s: %s", t.Instance, t.SchemaName, err)
-						skipped := len(ddls) - i
-						result.SkipCount += skipped
-						if skipped > 1 {
-							log.Warnf("Skipping %d remaining operations for %s %s due to previous error", skipped-1, t.Instance, t.SchemaName)
-						}
-						break
-					}
-				}
-			}
-
-			if targetStmtCount == 0 {
-				log.Infof("%s %s: No differences found\n", t.Instance, t.SchemaName)
-			} else {
-				verb := "push"
-				if dryRun {
-					verb = "diff"
-				}
-				log.Infof("%s %s: %s complete\n", t.Instance, t.SchemaName, verb)
-			}
+			results <- result
 
 			// Exit early if context cancelled
 			select {
@@ -151,8 +61,100 @@ func Worker(ctx context.Context, targetGroups <-chan TargetGroup, results chan<-
 			}
 		}
 	}
-	results <- result
 	return nil
+}
+
+func applyTarget(t *Target, printer *Printer) (Result, error) {
+	var result Result
+
+	schemaFromInstance, err := t.SchemaFromInstance()
+	if err != nil {
+		result.SkipCount++
+		log.Errorf("Skipping %s schema %s for %s: %s", t.Instance, t.SchemaName, t.Dir, err)
+		return result, err
+	}
+
+	t.logApplyStart()
+	diff := tengo.NewSchemaDiff(schemaFromInstance, t.SchemaFromDir())
+	if err := VerifyDiff(diff, t); err != nil {
+		return result, err
+	}
+
+	// Obtain StatementModifiers based on the dir's config
+	mods, err := StatementModifiersForDir(t.Dir)
+	if err != nil {
+		return result, ConfigError(err.Error())
+	}
+	mods.Flavor = t.Instance.Flavor()
+
+	// Build DDLStatements for each ObjectDiff, handling pre-execution errors
+	// accordingly. Also track ObjectKeys for modified objects, for subsequent
+	// use in linting.
+	objDiffs := diff.ObjectDiffs()
+	ddls := make([]*DDLStatement, 0, len(objDiffs))
+	keys := make([]tengo.ObjectKey, 0, len(objDiffs))
+	for _, objDiff := range objDiffs {
+		ddl, err := NewDDLStatement(objDiff, mods, t)
+		if ddl == nil && err == nil {
+			continue // Skip entirely if mods made the statement a noop
+		}
+		result.Differences = true
+		if err == nil {
+			ddls = append(ddls, ddl)
+			keys = append(keys, objDiff.ObjectKey())
+		} else if unsupportedErr, ok := err.(*tengo.UnsupportedDiffError); ok {
+			result.UnsupportedCount++
+			log.Warnf("Skipping %s: unable to generate DDL due to use of unsupported features. Use --debug for more information.", unsupportedErr.ObjectKey)
+			DebugLogUnsupportedDiff(unsupportedErr)
+		} else {
+			result.SkipCount += len(objDiffs)
+			log.Errorf(err.Error())
+			if len(objDiffs) > 1 {
+				log.Warnf("Skipping %d additional operations for %s %s due to previous error", len(objDiffs)-1, t.Instance, t.SchemaName)
+			}
+			return result, nil
+		}
+	}
+
+	// Lint any modified objects; output the result; skip target if any
+	// annotations are at the error level
+	if t.Dir.Config.GetBool("lint") {
+		lintOpts, err := linter.OptionsForDir(t.Dir)
+		if err != nil {
+			return result, ConfigError(err.Error())
+		}
+		lintOpts.OnlyKeys(keys)
+		lintResult := linter.CheckSchema(t.DesiredSchema, lintOpts)
+		lintResult.SortByFile()
+		for _, annotation := range lintResult.Annotations {
+			annotation.Log()
+		}
+		if lintResult.ErrorCount > 0 {
+			result.SkipCount += len(objDiffs)
+			log.Warnf("Skipping %s %s due to %s", t.Instance, t.SchemaName, countAndNoun(lintResult.ErrorCount, "linter error"))
+			return result, nil
+		}
+	}
+
+	// Print DDL; if not dry-run, execute it; final logging; return result
+	result.SkipCount += t.processDDL(ddls, printer)
+	t.logApplyEnd(result)
+	return result, nil
+}
+
+// supply 1 noun if pluralization is just adding an s, or 2 nouns if using
+// another word entirely
+func countAndNoun(n int, nouns ...string) string {
+	if n == 1 {
+		return fmt.Sprintf("1 %s", nouns[0])
+	}
+	var plural string
+	if len(nouns) == 1 {
+		plural = fmt.Sprintf("%ss", nouns[0])
+	} else {
+		plural = nouns[1]
+	}
+	return fmt.Sprintf("%d %s", n, plural)
 }
 
 // SumResults adds up the supplied results to return a single combined result.

applier/ddlstatement.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/skeema/mybase"
 	"github.com/skeema/skeema/fs"
 	"github.com/skeema/skeema/util"
 	"github.com/skeema/tengo"
@@ -35,43 +36,145 @@ func NewDDLStatement(diff tengo.ObjectDiff, mods tengo.StatementModifiers, targe
 		schemaName: target.SchemaName,
 	}
 
-	var tableSize int64
-	otype := diff.ObjectKey().Type
-	switch otype {
-	case tengo.ObjectTypeDatabase:
-		// Don't run database-level DDL in a schema; not even possible for CREATE
-		// DATABASE anyway
+	// Don't run database-level DDL in a schema; not even possible for CREATE
+	// DATABASE anyway
+	if diff.ObjectKey().Type == tengo.ObjectTypeDatabase {
 		ddl.schemaName = ""
-	case tengo.ObjectTypeTable:
-		// Obtain table size only if actually needed
-		needSize := anyOptChanged(target, "safe-below-size", "alter-wrapper-min-size") || wrapperUsesSize(target, "alter-wrapper", "ddl-wrapper")
-		if diff.DiffType() != tengo.DiffTypeCreate && needSize {
-			if tableSize, err = ddl.getTableSize(target, diff.(*tengo.TableDiff).From); err != nil {
-				return nil, err
-			}
-		}
 	}
 
-	// If --safe-below-size option in use, enable additional statement modifier
-	// if the table's size is less than the supplied option value
-	safeBelowSize, err := target.Dir.Config.GetBytes("safe-below-size")
-	if err != nil {
-		return nil, err
-	}
-	if otype == tengo.ObjectTypeTable && tableSize < int64(safeBelowSize) {
-		mods.AllowUnsafe = true
-		log.Debugf("Allowing unsafe operations for %s: size=%d < safe-below-size=%d", diff.ObjectKey(), tableSize, safeBelowSize)
+	// Get table size, but only if actually needed; apply --safe-below-size if
+	// specified
+	var tableSize int64
+	if needTableSize(diff, target.Dir.Config) {
+		if tableSize, err = getTableSize(target, diff.ObjectKey().Name); err != nil {
+			return nil, err
+		}
+
+		// If --safe-below-size option in use, enable additional statement modifier
+		// if the table's size is less than the supplied option value
+		if safeBelowSize, err := target.Dir.Config.GetBytes("safe-below-size"); err != nil {
+			return nil, err
+		} else if tableSize < int64(safeBelowSize) {
+			mods.AllowUnsafe = true
+			log.Debugf("Allowing unsafe operations for %s: size=%d < safe-below-size=%d", diff.ObjectKey(), tableSize, safeBelowSize)
+		}
 	}
 
 	// Options may indicate some/all DDL gets executed by shelling out to another program.
-	wrapper := target.Dir.Config.Get("ddl-wrapper")
-	if otype == tengo.ObjectTypeTable && diff.DiffType() == tengo.DiffTypeAlter && target.Dir.Config.Changed("alter-wrapper") {
-		minSize, err := target.Dir.Config.GetBytes("alter-wrapper-min-size")
+	wrapper, err := getWrapper(target.Dir.Config, diff, tableSize, &mods)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the raw DDL statement as a string, handling errors and noops correctly
+	if ddl.stmt, err = diff.Statement(mods); tengo.IsForbiddenDiff(err) {
+		errorText := fmt.Sprintf("Destructive statement /* %s */ is considered unsafe. Use --allow-unsafe or --safe-below-size to permit this operation; see --help for more information.", ddl.stmt)
+		return nil, errors.New(errorText)
+	} else if err != nil {
+		// Leave the error untouched/unwrapped to allow caller to handle appropriately
+		return nil, err
+	} else if ddl.stmt == "" {
+		// Noop statements (due to mods) must be skipped by caller
+		return nil, nil
+	}
+
+	if wrapper == "" {
+		ddl.connectParams = getConnectParams(diff, target.Dir.Config)
+	} else {
+		var socket, port, connOpts string
+		if ddl.instance.SocketPath != "" {
+			socket = ddl.instance.SocketPath
+		} else {
+			port = strconv.Itoa(ddl.instance.Port)
+		}
+		if connOpts, err = util.RealConnectOptions(target.Dir.Config.Get("connect-options")); err != nil {
+			return nil, ConfigError(err.Error())
+		}
+		variables := map[string]string{
+			"HOST":        ddl.instance.Host,
+			"PORT":        port,
+			"SOCKET":      socket,
+			"SCHEMA":      ddl.schemaName,
+			"USER":        target.Dir.Config.Get("user"),
+			"PASSWORD":    target.Dir.Config.Get("password"),
+			"ENVIRONMENT": target.Dir.Config.Get("environment"),
+			"DDL":         ddl.stmt,
+			"CLAUSES":     "", // filled in below only for tables
+			"NAME":        diff.ObjectKey().Name,
+			"TABLE":       "", // filled in below only for tables
+			"SIZE":        strconv.FormatInt(tableSize, 10),
+			"TYPE":        diff.DiffType().String(),
+			"CLASS":       diff.ObjectKey().Type.Caps(),
+			"CONNOPTS":    connOpts,
+			"DIRNAME":     target.Dir.BaseName(),
+			"DIRPATH":     target.Dir.Path,
+		}
+		if diff.ObjectKey().Type == tengo.ObjectTypeTable {
+			td := diff.(*tengo.TableDiff)
+			variables["CLAUSES"], _ = td.Clauses(mods)
+			variables["TABLE"] = variables["NAME"]
+		}
+
+		if ddl.shellOut, err = util.NewInterpolatedShellOut(wrapper, variables); err != nil {
+			errorText := fmt.Sprintf("A fatal error occurred with pre-processing a DDL statement: %s.", err)
+			return nil, errors.New(errorText)
+		}
+	}
+
+	return ddl, nil
+}
+
+// needTableSize returns true if diff represents an ALTER TABLE or DROP TABLE,
+// and at least one size-related option is in use, meaning that it will be
+// necessary to query for the table's size.
+func needTableSize(diff tengo.ObjectDiff, config *mybase.Config) bool {
+	if diff.ObjectKey().Type != tengo.ObjectTypeTable {
+		return false
+	}
+	if diff.DiffType() == tengo.DiffTypeCreate {
+		return false
+	}
+
+	// If safe-below-size or alter-wrapper-min-size options in use, size is needed
+	for _, opt := range []string{"safe-below-size", "alter-wrapper-min-size"} {
+		if config.Changed(opt) {
+			return true
+		}
+	}
+
+	// If any wrapper option uses the {SIZE} variable placeholder, size is needed
+	for _, opt := range []string{"alter-wrapper", "ddl-wrapper"} {
+		if strings.Contains(strings.ToUpper(config.Get(opt)), "{SIZE}") {
+			return true
+		}
+	}
+
+	return false
+}
+
+// getTableSize returns the size of the table on the instance corresponding to
+// the target. If the table has no rows, this method always returns a size of 0,
+// even though information_schema normally indicates at least 16kb in this case.
+func getTableSize(target *Target, tableName string) (int64, error) {
+	hasRows, err := target.Instance.TableHasRows(target.SchemaName, tableName)
+	if !hasRows || err != nil {
+		return 0, err
+	}
+	return target.Instance.TableSize(target.SchemaName, tableName)
+}
+
+// getWrapper returns the command-line for executing diff as a shell-out, if
+// configured to do so. Any variable placeholders in the returned string have
+// NOT been interpolated yet.
+func getWrapper(config *mybase.Config, diff tengo.ObjectDiff, tableSize int64, mods *tengo.StatementModifiers) (string, error) {
+	wrapper := config.Get("ddl-wrapper")
+	if diff.ObjectKey().Type == tengo.ObjectTypeTable && diff.DiffType() == tengo.DiffTypeAlter && config.Changed("alter-wrapper") {
+		minSize, err := config.GetBytes("alter-wrapper-min-size")
 		if err != nil {
-			return nil, err
+			return "", ConfigError(err.Error())
 		}
 		if tableSize >= int64(minSize) {
-			wrapper = target.Dir.Config.Get("alter-wrapper")
+			wrapper = config.Get("alter-wrapper")
 
 			// If alter-wrapper-min-size is set, and the table is big enough to use
 			// alter-wrapper, disable --alter-algorithm and --alter-lock. This allows
@@ -90,99 +193,28 @@ func NewDDLStatement(diff tengo.ObjectDiff, mods tengo.StatementModifiers, targe
 			log.Debugf("Skipping alter-wrapper for %s: size=%d < alter-wrapper-min-size=%d", diff.ObjectKey(), tableSize, minSize)
 		}
 	}
+	return wrapper, nil
+}
 
-	// Get the raw DDL statement as a string, handling errors and noops correctly
-	if ddl.stmt, err = diff.Statement(mods); tengo.IsForbiddenDiff(err) {
-		errorText := fmt.Sprintf("Destructive statement /* %s */ is considered unsafe. Use --allow-unsafe or --safe-below-size to permit this operation; see --help for more information.", ddl.stmt)
-		return nil, errors.New(errorText)
-	} else if err != nil {
-		// Leave the error untouched/unwrapped to allow caller to handle appropriately
-		return nil, err
-	} else if ddl.stmt == "" {
-		// Noop statements (due to mods) must be skipped by caller
-		return nil, nil
-	}
-
+// getConnectParams returns the necessary connection params (session variables)
+// for the supplied diff and config.
+func getConnectParams(diff tengo.ObjectDiff, config *mybase.Config) string {
 	// If adding foreign key constraints, use foreign_key_checks=1 if requested
-	if wrapper == "" && otype == tengo.ObjectTypeTable && diff.DiffType() == tengo.DiffTypeAlter &&
-		strings.Contains(ddl.stmt, "ADD CONSTRAINT") &&
-		strings.Contains(ddl.stmt, "FOREIGN KEY") &&
-		target.Dir.Config.GetBool("foreign-key-checks") {
-		ddl.connectParams = "foreign_key_checks=1"
+	if td, ok := diff.(*tengo.TableDiff); ok && config.GetBool("foreign-key-checks") {
+		_, addFKs := td.SplitAddForeignKeys()
+		if addFKs != nil {
+			return "foreign_key_checks=1"
+		}
 	}
 
 	// If creating a routine, use the server's global sql_mode instead of Skeema's
 	// normal built-in override
-	if wrapper == "" && (otype == tengo.ObjectTypeProc || otype == tengo.ObjectTypeFunc) &&
-		diff.DiffType() == tengo.DiffTypeCreate {
-		ddl.connectParams = "sql_mode=@@GLOBAL.sql_mode"
+	otype := diff.ObjectKey().Type
+	if diff.DiffType() == tengo.DiffTypeCreate && (otype == tengo.ObjectTypeProc || otype == tengo.ObjectTypeFunc) {
+		return "sql_mode=@@GLOBAL.sql_mode"
 	}
 
-	// Apply wrapper if relevant
-	if wrapper != "" {
-		var socket, port, connOpts string
-		if ddl.instance.SocketPath != "" {
-			socket = ddl.instance.SocketPath
-		} else {
-			port = strconv.Itoa(ddl.instance.Port)
-		}
-		if connOpts, err = util.RealConnectOptions(target.Dir.Config.Get("connect-options")); err != nil {
-			return nil, err
-		}
-		variables := map[string]string{
-			"HOST":        ddl.instance.Host,
-			"PORT":        port,
-			"SOCKET":      socket,
-			"SCHEMA":      ddl.schemaName,
-			"USER":        target.Dir.Config.Get("user"),
-			"PASSWORD":    target.Dir.Config.Get("password"),
-			"ENVIRONMENT": target.Dir.Config.Get("environment"),
-			"DDL":         ddl.stmt,
-			"CLAUSES":     "", // filled in below only for tables
-			"NAME":        diff.ObjectKey().Name,
-			"TABLE":       "", // filled in below only for tables
-			"SIZE":        strconv.FormatInt(tableSize, 10),
-			"TYPE":        diff.DiffType().String(),
-			"CLASS":       otype.Caps(),
-			"CONNOPTS":    connOpts,
-			"DIRNAME":     target.Dir.BaseName(),
-			"DIRPATH":     target.Dir.Path,
-		}
-		if otype == tengo.ObjectTypeTable {
-			td := diff.(*tengo.TableDiff)
-			variables["CLAUSES"], _ = td.Clauses(mods)
-			variables["TABLE"] = variables["NAME"]
-		}
-
-		if ddl.shellOut, err = util.NewInterpolatedShellOut(wrapper, variables); err != nil {
-			errorText := fmt.Sprintf("A fatal error occurred with pre-processing a DDL statement: %s.", err)
-			return nil, errors.New(errorText)
-		}
-	}
-
-	return ddl, nil
-}
-
-// anyOptChanged returns true if any of the specified option names have been
-// overridden from their default values for target's config
-func anyOptChanged(target *Target, options ...string) bool {
-	for _, opt := range options {
-		if target.Dir.Config.Changed(opt) {
-			return true
-		}
-	}
-	return false
-}
-
-// wrapperUsesSize returns true if any of the specified option names (which
-// should refer to "wrapper" command-lines) references the {SIZE} template var
-func wrapperUsesSize(target *Target, options ...string) bool {
-	for _, opt := range options {
-		if strings.Contains(strings.ToUpper(target.Dir.Config.Get(opt)), "{SIZE}") {
-			return true
-		}
-	}
-	return false
+	return ""
 }
 
 // IsShellOut returns true if the DDL is to be executed via shelling out to an
@@ -214,15 +246,4 @@ func (ddl *DDLStatement) Execute() error {
 	}
 	_, err = db.Exec(ddl.stmt)
 	return err
-}
-
-// getTableSize returns the size of the table on the instance corresponding to
-// the target. If the table has no rows, this method always returns a size of 0,
-// even though information_schema normally indicates at least 16kb in this case.
-func (ddl *DDLStatement) getTableSize(target *Target, table *tengo.Table) (int64, error) {
-	hasRows, err := target.Instance.TableHasRows(target.SchemaName, table.Name)
-	if !hasRows || err != nil {
-		return 0, err
-	}
-	return target.Instance.TableSize(target.SchemaName, table.Name)
 }

commit e7c9c76c845250b0eec768aab14a2c8537ab4073

diff/push: Fix output bug introduced yesterday
Commit 9f8c88a7 inadvertently had a flipped conditional, causing diff/push
to print summarizing "no differences found" message only when differences
WERE found.

Although that commit has a date of Aug 20, it didn't land in master until
Sep 4.

applier/target.go
@@ -63,13 +63,13 @@ func (t *Target) logApplyStart() {
 
 func (t *Target) logApplyEnd(result Result) {
 	if result.Differences {
-		log.Infof("%s %s: No differences found\n", t.Instance, t.SchemaName)
-	} else {
 		verb := "push"
 		if t.dryRun() {
 			verb = "diff"
 		}
 		log.Infof("%s %s: %s complete\n", t.Instance, t.SchemaName, verb)
+	} else {
+		log.Infof("%s %s: No differences found\n", t.Instance, t.SchemaName)
 	}
 }
 