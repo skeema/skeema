skeema/skeema

commit 00510a8120e6e84f2912482d348ed064b104ed99

Improve connection management and performance
This commit includes several small changes relating to connection-handling
across all Skeema commands.

Skeema now auto-detects when the database has been configured to use a
relatively low value for max_user_connections or max_connections. Skeema's
query concurrency is throttled down accordingly when this is the case.
Extremely low limits (e.g. 10 or less) may still be problematic for now
though.

The number of network round-trips per new connection has been reduced. This
should provide a performance benefit for users with nontrivial network
latency between Skeema and the database.

This commit also includes minor changes relating to the flavor option in rare
situations where the flavor cannot be auto-detected from an instance. In this
case, if the flavor is supplied on the command-line to `skeema init`, it will
be persisted into the host-level .skeema file. If the configured flavor
includes a patch version number, it will now be used, whereas previously the
patch number was inadvertently ignored.

go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/opencontainers/runc v1.0.0-rc5 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/skeema/mybase v1.0.13
-	github.com/skeema/tengo v0.9.8-0.20210208232911-9027677c0e25
+	github.com/skeema/tengo v0.9.8-0.20210212004258-b67f0f82b909
 	golang.org/x/lint v0.0.0-20190409202823-959b441ac422
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf

go.sum
@@ -49,8 +49,8 @@ github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMo
 github.com/fsouza/go-dockerclient v1.6.6 h1:9e3xkBrVkPb81gzYq23i7iDUEd6sx2ooeJA/gnYU6R4=
 github.com/fsouza/go-dockerclient v1.6.6/go.mod h1:3/oRIWoe7uT6bwtAayj/EmJmepBjeL4pYvt7ZxC7Rnk=
 github.com/go-sql-driver/mysql v1.4.0/go.mod h1:zAC/RDZ24gD3HViQzih4MyKcchzm+sOG5ZlKdlhCg5w=
-github.com/go-sql-driver/mysql v1.5.0 h1:ozyZYNQW3x3HtqT1jira07DN2PArx2v7/mN66gGcHOs=
-github.com/go-sql-driver/mysql v1.5.0/go.mod h1:DCzpHaOWr8IXmIStZouvnhqoel9Qv2LBy8hT2VhHyBg=
+github.com/go-sql-driver/mysql v1.5.1-0.20210202043019-fe2230a8b20c h1:yUT3Ygm3yXBD2qLPxYRDBcnEz0MHgQ4TJ/87C/wKnWA=
+github.com/go-sql-driver/mysql v1.5.1-0.20210202043019-fe2230a8b20c/go.mod h1:DCzpHaOWr8IXmIStZouvnhqoel9Qv2LBy8hT2VhHyBg=
 github.com/godbus/dbus v0.0.0-20190422162347-ade71ed3457e/go.mod h1:bBOAhwG1umN6/6ZUMtDFBMQR8jRg9O75tm9K00oMsK4=
 github.com/gogo/protobuf v1.2.1/go.mod h1:hp+jE20tsWTFYpLwKvXlhS1hjn+gTNwPg2I6zVXpSg4=
 github.com/gogo/protobuf v1.3.1 h1:DqDEcV5aeaTmdFBePNpYsp3FlcVH/2ISVVM9Qf8PSls=
@@ -130,8 +130,8 @@ github.com/sirupsen/logrus v1.4.2 h1:SPIRibHv4MatM3XXNO2BJeFLZwZ2LvZgfQ5+UNI2im4
 github.com/sirupsen/logrus v1.4.2/go.mod h1:tLMulIdttU9McNUspp0xgXVQah82FyeX6MwdIuYE2rE=
 github.com/skeema/mybase v1.0.13 h1:0wO6MyVexyvyRzghOaYpJaHnqoGbrTETpzeK0+W47Kk=
 github.com/skeema/mybase v1.0.13/go.mod h1:QIVXHvABKRem2FHkOv5lP8uQlQyc2x9GV4vIqq6tBcY=
-github.com/skeema/tengo v0.9.8-0.20210208232911-9027677c0e25 h1:QNj3My5V6VeDgxedppYq475pq2XeVyAULKuRMOTjzbk=
-github.com/skeema/tengo v0.9.8-0.20210208232911-9027677c0e25/go.mod h1:OXoEN3BZgDYLGfsNZEQrHWYWTYxI4/8dOVjGHaXug2k=
+github.com/skeema/tengo v0.9.8-0.20210212004258-b67f0f82b909 h1:s9Ge6sLBUJduRsOWXdQkisr7QsM225oCDcOfTKUKGQo=
+github.com/skeema/tengo v0.9.8-0.20210212004258-b67f0f82b909/go.mod h1:OwRHcRl8ltem6lEyP63of8o+6AhGwHeMhGwngNKHkQY=
 github.com/spf13/cobra v0.0.2-0.20171109065643-2da4a54c5cee/go.mod h1:1l0Ry5zgKvJasoi3XT1TypsSe7PqH0Sj9dhYf7v3XqQ=
 github.com/spf13/pflag v1.0.1-0.20171106142849-4c012f6dcd95/go.mod h1:DYY7MBk1bdzusC3SYhjObp+wFpr4gzcvqqNjLnInEg4=
 github.com/spf13/pflag v1.0.3/go.mod h1:DYY7MBk1bdzusC3SYhjObp+wFpr4gzcvqqNjLnInEg4=

skeema_test.go
@@ -450,7 +450,7 @@ func (s *SkeemaIntegrationSuite) dbExec(t *testing.T, schemaName, query string, 
 // it is fatal to the current test.
 func (s *SkeemaIntegrationSuite) dbExecWithParams(t *testing.T, schemaName, params, query string, args ...interface{}) {
 	t.Helper()
-	db, err := s.d.Connect(schemaName, params)
+	db, err := s.d.CachedConnectionPool(schemaName, params)
 	if err != nil {
 		t.Fatalf("Unable to connect to DockerizedInstance: %s", err)
 	}

vendor/github.com/skeema/tengo/go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/VividCortex/mysqlerr v0.0.0-20170204212430-6c6b55f8796f
 	github.com/fsouza/go-dockerclient v1.6.6
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-sql-driver/mysql v1.5.1-0.20210202043019-fe2230a8b20c
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.2.0 // indirect
 	github.com/mattn/go-sqlite3 v1.11.0 // indirect

vendor/github.com/skeema/tengo/go.sum
@@ -47,8 +47,8 @@ github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMo
 github.com/fsouza/go-dockerclient v1.6.6 h1:9e3xkBrVkPb81gzYq23i7iDUEd6sx2ooeJA/gnYU6R4=
 github.com/fsouza/go-dockerclient v1.6.6/go.mod h1:3/oRIWoe7uT6bwtAayj/EmJmepBjeL4pYvt7ZxC7Rnk=
 github.com/go-sql-driver/mysql v1.4.0/go.mod h1:zAC/RDZ24gD3HViQzih4MyKcchzm+sOG5ZlKdlhCg5w=
-github.com/go-sql-driver/mysql v1.5.0 h1:ozyZYNQW3x3HtqT1jira07DN2PArx2v7/mN66gGcHOs=
-github.com/go-sql-driver/mysql v1.5.0/go.mod h1:DCzpHaOWr8IXmIStZouvnhqoel9Qv2LBy8hT2VhHyBg=
+github.com/go-sql-driver/mysql v1.5.1-0.20210202043019-fe2230a8b20c h1:yUT3Ygm3yXBD2qLPxYRDBcnEz0MHgQ4TJ/87C/wKnWA=
+github.com/go-sql-driver/mysql v1.5.1-0.20210202043019-fe2230a8b20c/go.mod h1:DCzpHaOWr8IXmIStZouvnhqoel9Qv2LBy8hT2VhHyBg=
 github.com/godbus/dbus v0.0.0-20190422162347-ade71ed3457e/go.mod h1:bBOAhwG1umN6/6ZUMtDFBMQR8jRg9O75tm9K00oMsK4=
 github.com/gogo/protobuf v1.2.1/go.mod h1:hp+jE20tsWTFYpLwKvXlhS1hjn+gTNwPg2I6zVXpSg4=
 github.com/gogo/protobuf v1.3.1 h1:DqDEcV5aeaTmdFBePNpYsp3FlcVH/2ISVVM9Qf8PSls=

cmd_init.go
@@ -146,9 +146,7 @@ func createHostOptionFile(cfg *mybase.Config, hostDir *fs.Dir, inst *tengo.Insta
 	} else {
 		hostOptionFile.SetOptionValue(environment, "port", strconv.Itoa(inst.Port))
 	}
-	if flavor := inst.Flavor(); !flavor.Known() {
-		log.Warnf("Unable to automatically determine database vendor/version. To set manually, use the \"flavor\" option in %s", hostOptionFile)
-	} else {
+	if flavor := inst.Flavor(); flavor.Known() {
 		hostOptionFile.SetOptionValue(environment, "flavor", flavor.Family().String())
 	}
 	for _, persistOpt := range []string{"user", "ignore-schema", "ignore-table", "connect-options"} {

internal/applier/ddlstatement.go
@@ -250,7 +250,7 @@ func (ddl *DDLStatement) Execute() error {
 	if ddl.IsShellOut() {
 		return ddl.shellOut.Run()
 	}
-	db, err := ddl.instance.Connect(ddl.schemaName, ddl.connectParams)
+	db, err := ddl.instance.CachedConnectionPool(ddl.schemaName, ddl.connectParams)
 	if err != nil {
 		return err
 	}

commit c51a2cd1152b95f731346bdea2878f12fb5f7571

Test suite fixes; Docker fixes
Recent commit 00510a8 inadvertently caused sporadic random connection errors
with newly-created Dockerized databases, both in integration tests as well as
when using workspace=docker. This commit brings in the latest Tengo to fix.

This commit also fixes a sporadic integration test issue that randomly occurs
if Docker assigns the same port number to two containers in a row. The
relevant cache is now cleared completely in between switching images in the
test suite.

Finally, now that Golang 1.16 is available, this commit updates CI to use
Golang 1.15 instead of 1.14. (CI intentionally uses the older of the two
supported versions of Golang at any given time.)

.github/workflows/tests.yml
@@ -1,7 +1,7 @@
 name: Tests
 on: [push, pull_request]
 env:
-  GOVERSION: "1.14"
+  GOVERSION: "1.15"
   SKEEMA_TEST_IMAGES: "mysql:5.7,mysql:8.0"
 jobs:
   test:

go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/opencontainers/runc v1.0.0-rc5 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/skeema/mybase v1.0.13
-	github.com/skeema/tengo v0.9.8-0.20210219002552-a3e81979c678
+	github.com/skeema/tengo v0.9.8-0.20210219060639-01dd08dc1c6f
 	golang.org/x/lint v0.0.0-20190409202823-959b441ac422
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf

go.sum
@@ -130,8 +130,8 @@ github.com/sirupsen/logrus v1.4.2 h1:SPIRibHv4MatM3XXNO2BJeFLZwZ2LvZgfQ5+UNI2im4
 github.com/sirupsen/logrus v1.4.2/go.mod h1:tLMulIdttU9McNUspp0xgXVQah82FyeX6MwdIuYE2rE=
 github.com/skeema/mybase v1.0.13 h1:0wO6MyVexyvyRzghOaYpJaHnqoGbrTETpzeK0+W47Kk=
 github.com/skeema/mybase v1.0.13/go.mod h1:QIVXHvABKRem2FHkOv5lP8uQlQyc2x9GV4vIqq6tBcY=
-github.com/skeema/tengo v0.9.8-0.20210219002552-a3e81979c678 h1:9L2gzIck1/esgrAX7qISktRcIh8Mpb/D4lu77oRlvyg=
-github.com/skeema/tengo v0.9.8-0.20210219002552-a3e81979c678/go.mod h1:zrvIPs4+lw2VBJ2XX/tQj+gPbFheMht4AZFKGEhihYI=
+github.com/skeema/tengo v0.9.8-0.20210219060639-01dd08dc1c6f h1:s0y5TR+krXxtZe3EdwAA3oBnfN14uX/MFoRoT8FwWuI=
+github.com/skeema/tengo v0.9.8-0.20210219060639-01dd08dc1c6f/go.mod h1:zrvIPs4+lw2VBJ2XX/tQj+gPbFheMht4AZFKGEhihYI=
 github.com/spf13/cobra v0.0.2-0.20171109065643-2da4a54c5cee/go.mod h1:1l0Ry5zgKvJasoi3XT1TypsSe7PqH0Sj9dhYf7v3XqQ=
 github.com/spf13/pflag v1.0.1-0.20171106142849-4c012f6dcd95/go.mod h1:DYY7MBk1bdzusC3SYhjObp+wFpr4gzcvqqNjLnInEg4=
 github.com/spf13/pflag v1.0.3/go.mod h1:DYY7MBk1bdzusC3SYhjObp+wFpr4gzcvqqNjLnInEg4=

internal/util/cache.go
@@ -42,3 +42,12 @@ func CloseCachedConnectionPools() {
 		inst.CloseAll()
 	}
 }
+
+// FlushInstanceCache closes all connection pools in all cached Instances
+// that were created via NewInstance, and then flushes the cache entirely.
+func FlushInstanceCache() {
+	instanceCache.Lock()
+	defer instanceCache.Unlock()
+	CloseCachedConnectionPools()
+	instanceCache.instanceMap = make(map[string]*tengo.Instance)
+}

vendor/github.com/skeema/tengo/instance.go
@@ -192,19 +192,9 @@ func (instance *Instance) rawConnectionPool(defaultSchema, fullParams string, al
 // its configured User and Password. If a new connection cannot be made, the
 // return value will be false, along with an error expressing the reason.
 func (instance *Instance) CanConnect() (bool, error) {
-	// If we've already connected before, bypass the existing cached connection
-	// pools entirely to ensure we're not reusing an idle connection from a cached
-	// pool. Otherwise, it's safe to go through CachedConnectionPool and save this
-	// conn for future reuse for other purposes.
-	var err error
-	if len(instance.connectionPool) > 0 {
-		var db *sqlx.DB
-		db, err = instance.ConnectionPool("", "")
-		if db != nil {
-			db.Close() // close immediately to avoid a buildup of sleeping idle conns
-		}
-	} else {
-		_, err = instance.CachedConnectionPool("", "")
+	db, err := instance.ConnectionPool("", "")
+	if db != nil {
+		db.Close() // close immediately to avoid a buildup of sleeping idle conns
 	}
 	return err == nil, err
 }

vendor/github.com/skeema/tengo/testing.go
@@ -2,7 +2,6 @@ package tengo
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"reflect"
 	"strings"
@@ -45,8 +44,7 @@ func RunSuite(suite IntegrationTestSuite, t *testing.T, backends []string) {
 
 	for _, backend := range backends {
 		if err := suite.Setup(backend); err != nil {
-			log.Printf("Skipping integration test suite %s due to setup failure: %s", suiteName, err)
-			t.Skipf("RunSuite %s: Setup(%s) failed: %s", suiteName, backend, err)
+			t.Fatalf("RunSuite %s: Setup(%s) failed: %s", suiteName, backend, err)
 		}
 
 		// Run test methods

internal/applier/applier_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/skeema/skeema/internal/util"
 	"github.com/skeema/tengo"
 	"golang.org/x/sync/errgroup"
 )
@@ -96,7 +97,9 @@ func (s *ApplierIntegrationSuite) Teardown(backend string) error {
 			return s.d[n].Destroy()
 		})
 	}
-	return g.Wait()
+	err := g.Wait()
+	util.FlushInstanceCache()
+	return err
 }
 
 func (s *ApplierIntegrationSuite) BeforeTest(backend string) error {

skeema_test.go
@@ -76,6 +76,7 @@ func (s *SkeemaIntegrationSuite) Teardown(backend string) error {
 	if err := os.RemoveAll(s.scratchPath()); err != nil {
 		return err
 	}
+	util.FlushInstanceCache()
 	return nil
 }
 