281406287fee0beef5af89c7ed442e3041e947b2
Evan Elias <evan@skeema.net>
2021-07-14 16:28:23 -0400

    lint-pk: explain problem with nullable UNIQUE KEY
    The lint-pk checker flags any table that lacks an *explicit* PRIMARY KEY, even
    if it has an implicit one. That behavior remains as-is, but this commit
    improves the explanatory text for InnoDB tables with UNIQUE KEYs that *cannot*
    be used as an *implicit* PK.
    
    When an InnoDB table has no explicit PK, the engine will inherently use the
    first non-nullable UNIQUE KEY as the clustered index key (implicit PK), if one
    exists. In this case, there's no real harm to performance, and tools like
    pt-osc can still be used on the table. For this reason, lint-pk displays a
    shorter message in this situation; some DBAs are OK with this situation while
    others still prefer an explicit PK for clarity's sake. In any case, no changes
    to that behavior in this commit.
    
    However, the situation where a UNIQUE KEY is present, but non-usable as
    clustered index key due to allowing NULLs, can be confusing since it's far
    from obvious. In this case, tools like pt-osc cannot even be used. This commit
    adds additional explanation text to lint-pk to make this more clear.
    
    This commit also updates the vendored version of Go La Tengo to bring in a
    semi-related fix for diffing non-InnoDB tables.

--- internal/linter/check_pk.go
+++ internal/linter/check_pk.go
@@ -22,6 +22,12 @@ func pkChecker(table *tengo.Table, _ string, _ *tengo.Schema, _ Options) *Note {
 	var advice string
 	if table.Engine == "InnoDB" && table.ClusteredIndexKey() == nil {
 		advice = " Lack of a PRIMARY KEY hurts performance, and prevents use of third-party tools such as pt-online-schema-change."
+		for _, idx := range table.SecondaryIndexes {
+			if idx.Unique {
+				advice += " (Although this table does have a UNIQUE index, it cannot serve as the clustered index key either, since that requires use of only NOT NULL columns.)"
+				break
+			}
+		}
 	}
 	message := fmt.Sprintf("Table %s does not define a PRIMARY KEY.%s", table.Name, advice)
 	return &Note{
--- internal/linter/testdata/validcfg/nopk.sql
+++ internal/linter/testdata/validcfg/nopk.sql
@@ -1,4 +1,5 @@
 CREATE TABLE nopk ( /* annotations:pk */
 	id int unsigned NOT NULL,
-	name varchar(30)
+	name varchar(30),
+  UNIQUE KEY name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
