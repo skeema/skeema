skeema/skeema

commit bf38edb6138eb934229d353dd213e2a4c9ee87af

diff/push: fix rare edge cases with MySQL expression metadata
There are four places in CREATE TABLE that can contain expressions: generated
columns, functional indexes, DEFAULT expressions, and CHECK constraints. In
all four cases, there are rare situations where MySQL's information_schema
metadata can be mangled/malformed relative to SHOW CREATE TABLE.

Skeema already properly handled malformed I_S metadata for CHECK constraints,
as well as a few cases in the other features. However, other rare cases were
not handled previously, which could lead to `skeema diff` and `skeema push`
flagging a table as unsupported for diff operations (skipping the table). This
commit fixes these specific cases by parsing portions of SHOW CREATE TABLE:

* Generated column expressions in MySQL 5.7 containing 4-byte characters
* DEFAULT expressions in MySQL 8 containing string literals with charset
  introducers and/or multi-byte characters
* Functional index expressions in MySQL 8 containing string literals

During the investigation for this fix, several additional related unsupported-
for-diff situations were uncovered that have no known fix. These are cases
where metadata is mangled in *both* information_schema *and* SHOW CREATE
TABLE, likely due to the database server internally using utf8mb3:

* 4-byte characters in literal (non-expression) DEFAULT values, in all
  versions of MySQL and MariaDB: the DEFAULT value works properly, but cannot
  be introspected correctly
* 4-byte characters in COMMENT clauses, in all versions of MySQL and MariaDB:
  the database does not appear to store the value correctly at creation time
* 4-byte characters in generated column expressions in all versions of
  MariaDB: the database server does not generate the correct value
* 4-byte characters in CHECK constraints in all versions of MariaDB: feature
  works properly but cannot be introspected correctly
* 4-byte characters in DEFAULT expressions in all versions of MariaDB: feature

internal/tengo/introspect.go
@@ -116,12 +116,17 @@ func querySchemaTables(ctx context.Context, db *sqlx.DB, schema string, flavor F
 			fixForeignKeyOrder(t)
 		}
 		// Create options order is unpredictable with the new MySQL 8 data dictionary
-		// Also need to fix generated column expression string literals
 		// Also need to fix superfluous column charsets and utf8mb3->utf8 change
 		if flavor.HasDataDictionary() {
 			fixCreateOptionsOrder(t, flavor)
-			fixGenerationExpr(t, flavor)
 			fixShowCreateCharSets(t, flavor)
+		}
+		// MySQL 5.7+ generated column expressions must be reparased from SHOW CREATE
+		// TABLE to properly obtain any 4-byte chars. Additionally in 8.0 the I_S
+		// representation has incorrect escaping and potentially different charset
+		// in string literal introducers.
+		if flavor.MySQLishMinVersion(5, 7) {
+			fixGenerationExpr(t, flavor)
 		}
 		// Percona Server column compression can only be parsed from SHOW CREATE
 		// TABLE. (Although it also has new I_S tables, their name differs pre-8.0
@@ -134,9 +139,11 @@ func querySchemaTables(ctx context.Context, db *sqlx.DB, schema string, flavor F
 		if strings.Contains(t.CreateStatement, "WITH PARSER") {
 			fixFulltextIndexParsers(t, flavor)
 		}
-		// Fix blob/text default expressions in MySQL 8.0.13-8.0.22, missing from I_S
-		if flavor.MySQLishMinVersion(8, 0, 13) && !flavor.MySQLishMinVersion(8, 0, 23) {
-			fixBlobDefaultExpression(t, flavor)
+		// Fix problems with I_S data for default expressions as well as functional
+		// indexes in MySQL 8
+		if flavor.MySQLishMinVersion(8, 0) {
+			fixDefaultExpression(t, flavor)
+			fixIndexExpression(t, flavor)
 		}
 		// Fix shortcoming in I_S data for check constraints
 		if len(t.Checks) > 0 {
@@ -277,7 +284,7 @@ func queryColumnsInSchema(ctx context.Context, db *sqlx.DB, schema string, flavo
 				// information_schema due to a bug, so in this case flag the column to
 				// have its default parsed out from SHOW CREATE TABLE later
 				if strings.Contains(rawColumn.Extra, "DEFAULT_GENERATED") {
-					col.Default = "!!!BLOBDEFAULT!!!"
+					col.Default = "(!!!BLOBDEFAULT!!!)"
 				}
 			}
 			if allowNullDefault {
@@ -293,10 +300,10 @@ func queryColumnsInSchema(ctx context.Context, db *sqlx.DB, schema string, flavo
 		} else if strings.HasPrefix(rawColumn.Type, "bit") && strings.HasPrefix(rawColumn.Default.String, "b'") {
 			col.Default = rawColumn.Default.String
 		} else if strings.Contains(rawColumn.Extra, "DEFAULT_GENERATED") {
-			// MySQL/Percona 8.0.13+ added default expressions, which are paren-wrapped
-			// in SHOW CREATE TABLE, possibly in addition to any paren-wrapping which
-			// is already in information_schema. However, quotes get oddly mangled in
-			// information_schema's representation.
+			// MySQL 8.0.13+ supports default expressions, which are paren-wrapped in
+			// SHOW CREATE TABLE in MySQL. However MySQL I_S data has some issues for
+			// default expressions. The most common one is fixed here, and if additional
+			// mismatches remain, they get corrected by fixDefaultExpression later on.
 			col.Default = fmt.Sprintf("(%s)", strings.ReplaceAll(rawColumn.Default.String, "\\'", "'"))
 		} else {
 			col.Default = fmt.Sprintf("'%s'", EscapeValueForCreateTable(rawColumn.Default.String))
@@ -695,27 +702,29 @@ func fixShowCreateCharSets(t *Table, flavor Flavor) {
 	}
 }
 
-// MySQL 8 has nonsensical behavior regarding string literals in generated col
-// expressions: the literals are expressed using a different charset in SHOW
-// CREATE TABLE vs information_schema.columns.generation_expression. This method
-// modifies each generated Column.GenerationExpr to match SHOW CREATE's version.
+// MySQL 5.7+ supports generated columns, but mangles them in I_S in various
+// ways:
+// * 4-byte characters are not returned properly in I_S since it uses utf8mb3
+// * MySQL 8 incorrectly mangles escaping of single quotes in the I_S value
+// * MySQL 8 potentially uses different charsets introducers for string literals
+//   in I_S vs SHOW CREATE
+// This method modifies each generated Column.GenerationExpr to match SHOW
+// CREATE's version.
 func fixGenerationExpr(t *Table, flavor Flavor) {
 	for _, col := range t.Columns {
-		if col.GenerationExpr != "" {
-			// Approach: dynamically build a regexp that captures the generation expr
-			// from the correct line of the full SHOW CREATE TABLE output
-			origExpr := col.GenerationExpr
-			col.GenerationExpr = "!!!GENEXPR!!!"
-			reTemplate := regexp.QuoteMeta(col.Definition(flavor, t))
-			reTemplate = strings.Replace(reTemplate, col.GenerationExpr, "(.*)", -1)
-			re := regexp.MustCompile(reTemplate)
-			matches := re.FindStringSubmatch(t.CreateStatement)
-			if matches == nil {
-				// If we somehow failed to match correctly, fall back to using the
-				// uncorrected value from information_schema; unsupported diff is
-				// preferable to a nil pointer panic
-				col.GenerationExpr = origExpr
+		if col.GenerationExpr == "" {
+			continue
+		}
+		if colDefinition := col.Definition(flavor, t); !strings.Contains(t.CreateStatement, colDefinition) {
+			var genKind string
+			if col.Virtual {
+				genKind = "VIRTUAL"
 			} else {
+				genKind = "STORED"
+			}
+			reTemplate := `(?m)^\s*` + regexp.QuoteMeta(EscapeIdentifier(col.Name)) + `.+GENERATED ALWAYS AS \((.+)\) ` + genKind
+			re := regexp.MustCompile(reTemplate)
+			if matches := re.FindStringSubmatch(t.CreateStatement); matches != nil {
 				col.GenerationExpr = matches[1]
 			}
 		}
@@ -802,20 +811,56 @@ func fixFulltextIndexParsers(t *Table, flavor Flavor) {
 	}
 }
 
-// fixBlobDefaultExpression parses the table's CREATE string in order to
-// populate Column.Default for blob/text columns using a default expression
-// in MySQLish 8.0.13-8.0.22, which omits this from information_schema due
-// to a bug fixed in MySQL 8.0.23.
-func fixBlobDefaultExpression(t *Table, flavor Flavor) {
+// fixDefaultExpression parses the table's CREATE string in order to correct
+// problems in Column.Default for columns using a default expression in MySQL 8:
+// * In MySQL 8.0.13-8.0.22, blob/text cols may have default expressions but
+//   these are omitted from I_S due to a bug fixed in MySQL 8.0.23.
+// * 4-byte characters are not returned properly in I_S since it uses utf8mb3
+// * MySQL 8 incorrectly mangles escaping of single quotes in the I_S value
+// * MySQL 8 potentially uses different charsets introducers for string literals
+//   in I_S vs SHOW CREATE
+func fixDefaultExpression(t *Table, flavor Flavor) {
 	for _, col := range t.Columns {
-		if col.Default == "!!!BLOBDEFAULT!!!" {
-			template := col.Definition(flavor, t)
-			template = regexp.QuoteMeta(template)
-			template = fmt.Sprintf("%s,?\n", strings.Replace(template, col.Default, "(.+?)", 1))
-			re := regexp.MustCompile(template)
-			matches := re.FindStringSubmatch(t.CreateStatement)
-			if matches != nil {
+		if col.Default == "" || col.Default[0] != '(' {
+			continue
+		}
+		if colDefinition := col.Definition(flavor, t); !strings.Contains(t.CreateStatement, colDefinition) {
+			defaultClause := " DEFAULT " + col.Default
+			after := colDefinition[strings.Index(colDefinition, defaultClause)+len(defaultClause):]
+			reTemplate := `(?m)^\s*` + regexp.QuoteMeta(EscapeIdentifier(col.Name)) + `.+DEFAULT (\(.+\))` + after
+			re := regexp.MustCompile(reTemplate)
+			if matches := re.FindStringSubmatch(t.CreateStatement); matches != nil {
 				col.Default = matches[1]
+			}
+		}
+	}
+}
+
+// fixIndexExpression parses the table's CREATE string in order to correct
+// problems in index expressions (functional indexes) in MySQL 8:
+// * 4-byte characters are not returned properly in I_S since it uses utf8mb3
+// * MySQL 8 incorrectly mangles escaping of single quotes in the I_S value
+func fixIndexExpression(t *Table, flavor Flavor) {
+	// Only need to check secondary indexes, since PK can't contain expressions
+	for _, idx := range t.SecondaryIndexes {
+		if !idx.Functional() {
+			continue
+		}
+		if idxDefinition := idx.Definition(flavor); !strings.Contains(t.CreateStatement, idxDefinition) {
+			exprParts := make([]*IndexPart, 0, len(idx.Parts))
+			for n := range idx.Parts {
+				if idx.Parts[n].Expression != "" {
+					idxDefinition = strings.Replace(idxDefinition, idx.Parts[n].Expression, "!!!EXPR!!!", 1)
+					exprParts = append(exprParts, &idx.Parts[n])
+				}
+			}
+			// Build a regex which captures just the index expression(s) for this index
+			reTemplate := regexp.QuoteMeta(idxDefinition)
+			reTemplate = `(?m)^\s*` + strings.ReplaceAll(reTemplate, "!!!EXPR!!!", "(.*)") + `,?$`
+			re := regexp.MustCompile(reTemplate)
+			matches := re.FindStringSubmatch(t.CreateStatement)
+			for n := 1; n < len(matches); n++ {
+				exprParts[n-1].Expression = matches[n]
 			}
 		}
 	}

internal/tengo/docker.go
@@ -100,7 +100,9 @@ func (dc *DockerClient) CreateInstance(opts DockerizedInstanceOptions) (*Dockeri
 	// Use MYSQL_ROOT_HOST=% to ensure user root@% is created; Dockerhub "official"
 	// images do this by default, but mysql/mysql-server does not. Regardless of
 	// this, the HostConfig restricts connections to the host machine anyway.
-	env := []string{"MYSQL_ROOT_HOST=%"}
+	// Use LANG=C.UTF-8 to ensure client programs can pass multi-byte chars
+	// correctly, since DockerizedInstance.SourceSQL shells out to the client.
+	env := []string{"MYSQL_ROOT_HOST=%", "LANG=C.UTF-8"}
 	if opts.RootPassword == "" {
 		env = append(env, "MYSQL_ALLOW_EMPTY_PASSWORD=1")
 	} else {
@@ -328,7 +330,7 @@ func (di *DockerizedInstance) SourceSQL(filePath string) (string, error) {
 		return "", fmt.Errorf("SourceSQL %s: Unable to open setup file %s: %s", di, filePath, err)
 	}
 	defer f.Close()
-	cmd := []string{"mysql", "-tvvv", "-u", "root", "-h", "127.0.0.1"}
+	cmd := []string{"mysql", "-tvvv", "-u", "root", "-h", "127.0.0.1", "--default-character-set", "utf8mb4"}
 	if di.RootPassword != "" {
 		cmd = append(cmd, fmt.Sprintf("-p%s", di.RootPassword))
 	}

internal/tengo/index.go
@@ -148,6 +148,17 @@ func (idx *Index) RedundantTo(other *Index) bool {
 	return true
 }
 
+// Functional returns true if at least one IndexPart in idx is an expression
+// rather than a column.
+func (idx *Index) Functional() bool {
+	for _, part := range idx.Parts {
+		if part.Expression != "" {
+			return true
+		}
+	}
+	return false
+}
+
 // Definition returns this index part's definition clause.
 func (part *IndexPart) Definition(_ Flavor) string {
 	var base, prefix, collation string

internal/tengo/instance.go
@@ -509,10 +509,26 @@ func (instance *Instance) introspectionParams() string {
 	v := url.Values{}
 	v.Set("sql_quote_show_create", "1")
 
+	flavor := instance.Flavor()
+
 	// In MySQL 8, ensure we get up-to-date values for table sizes as well as next
 	// auto_increment value
-	if instance.Flavor().HasDataDictionary() {
+	if flavor.HasDataDictionary() {
 		v.Set("information_schema_stats_expiry", "0")
+	}
+
+	// In MySQL, we need a binary collation in order for SHOW CREATE TABLE to
+	// correctly return 4-byte chars in generated column expressions (5.7+), column
+	// default expressions (8.0+), check constraint clauses (8.0+), and
+	// functional index expressions (8.0+). Note that this isn't a silver bullet:
+	// * Non-expression default value literals still don't show 4-byte chars
+	//   correctly, regardless of collation, in any flavor
+	// * information_schema does not return 4-byte chars properly, regardless of
+	//   collation; we must reparse from SHOW CREATE TABLE
+	// * In MariaDB, SHOW CREATE TABLE does not return 4-byte chars correctly
+	//   regardless of collation
+	if flavor.MySQLishMinVersion(5, 7) {
+		v.Set("collation", "binary")
 	}
 
 	keepMode := make([]string, 0, len(instance.sqlMode))

internal/tengo/instance_test.go
@@ -137,13 +137,13 @@ func TestInstanceIntrospectionParams(t *testing.T) {
 			t.Errorf("Expected param map %v, instead found %v", parsedExpected, parsedResult)
 		}
 	}
-	assertParams(FlavorMySQL57, "", "sql_quote_show_create=1")
-	assertParams(FlavorMySQL80, "", "sql_quote_show_create=1&information_schema_stats_expiry=0")
-	assertParams(FlavorMySQL57, "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE", "sql_quote_show_create=1")
-	assertParams(FlavorPercona80, "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE", "sql_quote_show_create=1&information_schema_stats_expiry=0")
+	assertParams(FlavorMySQL57, "", "sql_quote_show_create=1&collation=binary")
+	assertParams(FlavorMySQL80, "", "sql_quote_show_create=1&information_schema_stats_expiry=0&collation=binary")
+	assertParams(FlavorMySQL57, "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE", "sql_quote_show_create=1&collation=binary")
+	assertParams(FlavorPercona80, "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE", "sql_quote_show_create=1&information_schema_stats_expiry=0&collation=binary")
 	assertParams(FlavorMariaDB105, "ANSI_QUOTES", "sql_quote_show_create=1&sql_mode=%27%27")
-	assertParams(FlavorMySQL57, "REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE,ONLY_FULL_GROUP_BY,ANSI", "sql_quote_show_create=1&sql_mode=%27REAL_AS_FLOAT%2CPIPES_AS_CONCAT%2CIGNORE_SPACE%2CONLY_FULL_GROUP_BY%27")
-	assertParams(FlavorMySQL80, "NO_FIELD_OPTIONS,NO_BACKSLASH_ESCAPES,NO_KEY_OPTIONS,NO_TABLE_OPTIONS", "sql_quote_show_create=1&information_schema_stats_expiry=0&sql_mode=%27NO_BACKSLASH_ESCAPES%27")
+	assertParams(FlavorMySQL57, "REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE,ONLY_FULL_GROUP_BY,ANSI", "sql_quote_show_create=1&collation=binary&sql_mode=%27REAL_AS_FLOAT%2CPIPES_AS_CONCAT%2CIGNORE_SPACE%2CONLY_FULL_GROUP_BY%27")
+	assertParams(FlavorMySQL80, "NO_FIELD_OPTIONS,NO_BACKSLASH_ESCAPES,NO_KEY_OPTIONS,NO_TABLE_OPTIONS", "sql_quote_show_create=1&collation=binary&information_schema_stats_expiry=0&sql_mode=%27NO_BACKSLASH_ESCAPES%27")
 }
 
 func (s TengoIntegrationSuite) TestInstanceConnect(t *testing.T) {

internal/tengo/introspect_test.go
@@ -8,10 +8,11 @@ import (
 )
 
 func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
-	s.SourceTestSQL(t, "integration-ext.sql", "rows.sql")
+	// include broad coverage for many flavor-specific features
+	flavor := s.d.Flavor()
+	s.SourceTestSQL(t, flavorTestFiles(flavor)...)
 
 	// Ensure our unit test fixtures and integration test fixtures match
-	flavor := s.d.Flavor()
 	schema, aTableFromDB := s.GetSchemaAndTable(t, "testing", "actor")
 	aTableFromUnit := aTableForFlavor(flavor, 1)
 	aTableFromUnit.CreateStatement = "" // Prevent diff from short-circuiting on equivalent CREATEs
@@ -97,25 +98,37 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 
 	// Test introspection of default expressions, if flavor supports them
 	if flavor.VendorMinVersion(VendorMariaDB, 10, 2) || flavor.MySQLishMinVersion(8, 0, 13) {
-		s.SourceTestSQL(t, "default-expr.sql")
 		table := s.GetTable(t, "testing", "testdefaults")
-		if table.UnsupportedDDL {
-			t.Errorf("Use of default expression unexpectedly triggers UnsupportedDDL.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", table.GeneratedCreateStatement(flavor), table.CreateStatement)
+		// Ensure 3-byte chars in default expression are introspected properly
+		if !strings.Contains(table.CreateStatement, "\u20AC") {
+			t.Errorf("Expected default expression to contain 3-byte char \u20AC, but it did not. CREATE statement:\n%s", table.CreateStatement)
+		}
+
+		// In MySQL, ensure 4-byte chars in default expressions are introspected
+		// properly. (MariaDB supports them, but does not appear to provide any way
+		// to properly introspect them; they're mangled in both I_S and SHOW CREATE!)
+		if flavor.Vendor != VendorMariaDB && !strings.Contains(table.CreateStatement, "\U0001F4A9") {
+			t.Errorf("Expected default expression to contain 4-byte char \U0001F4A9, but it did not. CREATE statement:\n%s", table.CreateStatement)
 		}
 	}
 
 	// Test introspection of generated columns, if flavor supports them
 	if flavor.GeneratedColumns() {
-		sqlfile := "generatedcols.sql"
-		if flavor.Vendor == VendorMariaDB { // no support for NOT NULL generated cols
-			sqlfile = "generatedcols-maria.sql"
-		}
-		s.SourceTestSQL(t, sqlfile)
 		table := s.GetTable(t, "testing", "staff")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using generated columns to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
+		// Ensure 3-byte chars in generation expression are introspected properly
+		if !strings.Contains(table.CreateStatement, "\u20AC") {
+			t.Errorf("Expected generation expression to contain 3-byte char \u20AC, but it did not. CREATE statement:\n%s", table.CreateStatement)
 		}
-		// Test generation expression fix, even if test image isn't MySQL 8
+
+		// In MySQL, ensure 4-byte chars in default expressions are introspected
+		// properly. (MariaDB does not appear to handle them properly in generated
+		// column expressions at all, it's corrupted at creation time and on a
+		// functional level, not just introspection.)
+		if flavor.Vendor != VendorMariaDB && !strings.Contains(table.CreateStatement, "\U0001F4A9") {
+			t.Errorf("Expected generation expression to contain 4-byte char \U0001F4A9, but it did not. CREATE statement:\n%s", table.CreateStatement)
+		}
+
+		// Test generation expression fix, even if test image isn't MySQL 5.7+
 		for _, col := range table.Columns {
 			if col.GenerationExpr != "" {
 				col.GenerationExpr = "length(_latin1\\'fixme\\')"
@@ -129,10 +142,12 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 
 	// Test advanced index functionality in MySQL 8+
 	if flavor.MySQLishMinVersion(8, 0) {
-		s.SourceTestSQL(t, "index-mysql8.sql")
 		table := s.GetTable(t, "testing", "my8idx")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using advanced index functionality to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
+		if !strings.Contains(table.CreateStatement, "\u20AC") {
+			t.Errorf("Expected functional index expression to contain 3-byte char \u20AC, but it did not. CREATE statement:\n%s", table.CreateStatement)
+		}
+		if !strings.Contains(table.CreateStatement, "\U0001F4A9") {
+			t.Errorf("Expected functional index expression to contain 4-byte char \U0001F4A9, but it did not. CREATE statement:\n%s", table.CreateStatement)
 		}
 		idx := table.SecondaryIndexes[0]
 		if !idx.Invisible {
@@ -145,11 +160,7 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 			t.Errorf("Unexpected index part expressions found: [0].Expression=%q, [1].Expression=%q", idx.Parts[0].Expression, idx.Parts[1].Expression)
 		}
 	} else if flavor.VendorMinVersion(VendorMariaDB, 10, 6) {
-		s.SourceTestSQL(t, "index-maria106.sql")
 		table := s.GetTable(t, "testing", "maria106idx")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using ignored index to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
-		}
 		idx := table.SecondaryIndexes[0]
 		if !idx.Invisible {
 			t.Errorf("Expected index %s to be ignored, but it was not", idx.Name)
@@ -158,11 +169,7 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 
 	// Test invisible column support in flavors supporting it
 	if flavor.VendorMinVersion(VendorMariaDB, 10, 3) || flavor.MySQLishMinVersion(8, 0, 23) {
-		s.SourceTestSQL(t, "inviscols.sql")
 		table := s.GetTable(t, "testing", "invistest")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using invisible columns to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
-		}
 		for n, col := range table.Columns {
 			expectInvis := (n == 0 || n == 4 || n == 5)
 			if col.Invisible != expectInvis {
@@ -174,11 +181,7 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 	// Include coverage for fulltext parsers if MySQL 5.7+. (Although these are
 	// supported in other flavors too, no alternative parsers ship with them.)
 	if flavor.MySQLishMinVersion(5, 7) {
-		s.SourceTestSQL(t, "ft-parser.sql")
 		table := s.GetTable(t, "testing", "ftparser")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using ngram fulltext parser to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
-		}
 		indexes := table.SecondaryIndexesByName()
 		if idx := indexes["ftdesc"]; idx.FullTextParser != "ngram" || idx.Type != "FULLTEXT" {
 			t.Errorf("Expected index %s to be FULLTEXT with ngram parser, instead found type=%s / parser=%s", idx.Name, idx.Type, idx.FullTextParser)
@@ -193,20 +196,12 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 
 	// Coverage for column compression
 	if flavor.VendorMinVersion(VendorPercona, 5, 6, 33) {
-		s.SourceTestSQL(t, "colcompression-percona.sql")
 		table := s.GetTable(t, "testing", "colcompr")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using column compression to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
-		}
 		if table.Columns[1].Compression != "COMPRESSED" {
 			t.Errorf("Unexpected value for compression column attribute: found %q", table.Columns[1].Compression)
 		}
 	} else if flavor.VendorMinVersion(VendorMariaDB, 10, 3) {
-		s.SourceTestSQL(t, "colcompression-maria.sql")
 		table := s.GetTable(t, "testing", "colcompr")
-		if table.UnsupportedDDL {
-			t.Errorf("Expected table using column compression to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
-		}
 		if table.Columns[1].Compression != "COMPRESSED" {
 			t.Errorf("Unexpected value for compression column attribute: found %q", table.Columns[1].Compression)
 		}
@@ -214,18 +209,19 @@ func (s TengoIntegrationSuite) TestInstanceSchemaIntrospection(t *testing.T) {
 
 	// Include tables with check constraints if supported by flavor
 	if flavor.HasCheckConstraints() {
-		filename := "check.sql"
-		if flavor.Vendor == VendorMariaDB {
-			filename = "check-maria.sql"
-		}
-		s.SourceTestSQL(t, filename)
 		tablesWithChecks := []*Table{s.GetTable(t, "testing", "has_checks1"), s.GetTable(t, "testing", "has_checks2")}
 		for _, table := range tablesWithChecks {
 			if len(table.Checks) == 0 {
 				t.Errorf("Expected table %s to have at least one CHECK constraint, but it did not", table.Name)
 			}
-			if table.UnsupportedDDL {
-				t.Errorf("Expected table %s to be supported for diff in flavor %s, but it was not.\nExpected SHOW CREATE TABLE:\n%s\nActual SHOW CREATE TABLE:\n%s", table.Name, flavor, table.GeneratedCreateStatement(flavor), table.CreateStatement)
+			if table.Name == "has_checks1" {
+				if flavor.Vendor == VendorMariaDB {
+					if !strings.Contains(table.CreateStatement, "\u20AC") {
+						t.Errorf("Expected check constraint clause to contain 3-byte char \u20AC, but it did not. CREATE statement:\n%s", table.CreateStatement)
+					}
+				} else if !strings.Contains(table.CreateStatement, "\U0001F4A9") {
+					t.Errorf("Expected check constraint clause to contain 4-byte char \U0001F4A9, but it did not. CREATE statement:\n%s", table.CreateStatement)
+				}
 			}
 		}
 	}
@@ -263,6 +259,11 @@ func (s TengoIntegrationSuite) TestInstanceRoutineIntrospection(t *testing.T) {
 	}
 	if actualFunc1.Equals(actualProc1) {
 		t.Error("Equals not behaving as expected, proc1 and func1 should not be equal")
+	}
+
+	// confirm 4-byte characters in the body come through properly
+	if func2 := funcsByName["func2"]; !strings.Contains(func2.Body, "\U0001F4A9") {
+		t.Errorf("Expected to find 4-byte char \U0001F4A9 in func2.Body, but did not. Body contents:\n%s", func2.Body)
 	}
 
 	// If this flavor supports using mysql.proc to bulk-fetch routines, confirm
@@ -380,19 +381,19 @@ func TestFixBlobDefaultExpression(t *testing.T) {
 	defExpr := "(CONCAT('hello ', 'world'))"
 	table.Columns[1].Default = defExpr
 	table.CreateStatement = table.GeneratedCreateStatement(FlavorMySQL80)
-	table.Columns[1].Default = "!!!BLOBDEFAULT!!!"
-	fixBlobDefaultExpression(&table, FlavorMySQL80)
+	table.Columns[1].Default = "(!!!BLOBDEFAULT!!!)"
+	fixDefaultExpression(&table, FlavorMySQL80)
 	if table.Columns[1].Default != defExpr {
-		t.Errorf("fixBlobDefaultExpression did not work or set default to unexpected value %q", table.Columns[1].Default)
+		t.Errorf("fixDefaultExpression did not work or set default to unexpected value %q", table.Columns[1].Default)
 	}
 
 	// Confirm regex still correct with stuff after the default
 	table.Columns[1].Comment = "hi i am a comment"
 	table.CreateStatement = table.GeneratedCreateStatement(FlavorMySQL80)
-	table.Columns[1].Default = "!!!BLOBDEFAULT!!!"
-	fixBlobDefaultExpression(&table, FlavorMySQL80)
+	table.Columns[1].Default = "(!!!BLOBDEFAULT!!!)"
+	fixDefaultExpression(&table, FlavorMySQL80)
 	if table.Columns[1].Default != defExpr {
-		t.Errorf("fixBlobDefaultExpression did not work after adding comment, default is unexpected value %q", table.Columns[1].Default)
+		t.Errorf("fixDefaultExpression did not work after adding comment, default is unexpected value %q", table.Columns[1].Default)
 	}
 }
 

commit 27b379b66289c3aa9f7382fa546ce8380b9124c4

MySQL 8: fix potential DEFAULT expression panic
A bug in yesterday's commit bf38edb introduced a potential regexp.MustCompile
panic whenever a column has a DEFAULT expression and additional clauses after
that, such as a COMMENT clause, ON UPDATE clause, Percona column compression
clause, or INVISIBLE modifier. This commit fixes the panic by properly
escaping any regex meta characters in the subsequent clauses.

internal/tengo/introspect.go
@@ -827,7 +827,7 @@ func fixDefaultExpression(t *Table, flavor Flavor) {
 		if colDefinition := col.Definition(flavor, t); !strings.Contains(t.CreateStatement, colDefinition) {
 			defaultClause := " DEFAULT " + col.Default
 			after := colDefinition[strings.Index(colDefinition, defaultClause)+len(defaultClause):]
-			reTemplate := `(?m)^\s*` + regexp.QuoteMeta(EscapeIdentifier(col.Name)) + `.+DEFAULT (\(.+\))` + after
+			reTemplate := `(?m)^\s*` + regexp.QuoteMeta(EscapeIdentifier(col.Name)) + `.+DEFAULT (\(.+\))` + regexp.QuoteMeta(after)
 			re := regexp.MustCompile(reTemplate)
 			if matches := re.FindStringSubmatch(t.CreateStatement); matches != nil {
 				col.Default = matches[1]

internal/tengo/testdata/default-expr.sql
@@ -13,7 +13,7 @@ CREATE TABLE testdefaults (
 	b int default (a*a),
 	c int default (abs(a)),
 	d varchar(100) default 'hello',
-	e varchar(200) default (concat(d, ' world 💩')),
+	e varchar(200) default (concat(d, ' world 💩')) COMMENT '(stuff after default',
 	f varchar(30) NOT NULL default '',
 	g float default (rand()),
 	h float default (rand() * rand()),