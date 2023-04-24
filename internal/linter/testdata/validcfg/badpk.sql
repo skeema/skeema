17b9e0925c245dac4fddc9058287fc28df8b6373
Evan Elias <evan@skeema.net>
2023-04-24 19:33:23 -0400

    tests: fix lint-pk-type integration test for older flavors
    Integration tests in the linter subpackage were failing on MySQL 5.5 + 5.6,
    as well as MariaDB 10.1. With default settings, these old flavors limit
    primary key max length to 767 bytes. This commit adjusts varchar sizes of
    test tables to fit under the limit.

--- internal/linter/testdata/validcfg/badpk.sql
+++ internal/linter/testdata/validcfg/badpk.sql
@@ -1,11 +1,11 @@
 CREATE TABLE badpk (
- a varchar(255) not null primary key, /* annotations: pk-type */
+ a varchar(100) not null primary key, /* annotations: pk-type */
  b int not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE badpk_composite ( 
  a int not null,
- b varchar(255) not null,  /* annotations: pk-type */
+ b varchar(100) not null,  /* annotations: pk-type */
  c int not null,
  primary key (a,b)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
