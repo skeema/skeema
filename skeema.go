085259b2f833beb0588e72f9085c1f51bdfd5879
Evan Elias <evan@skeema.net>
2025-02-27 17:01:41 -0500

    version bump: release v1.12.3-community

--- skeema.go
+++ skeema.go
@@ -16,7 +16,7 @@ const rootDesc = "Skeema is a declarative schema management system for MySQL and
 
 // Globals overridden by GoReleaser's ldflags
 var (
-	version = "1.12.2"
+	version = "1.12.3"
 	commit  = "unknown"
 	date    = "unknown"
 )
