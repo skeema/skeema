skeema/skeema

commit 49b79d1ae65970bdb65dfb4207d31d69d90476a2

performance: improve connection reuse; refactor introspection
This commit reduces the number of discrete connection pools used by Skeema,
improving connection reuse, which is helpful for performance when nontrivial
network latency is present. Among the changes here:

* When `skeema push` executes DDL on the target database, the same connection
  is now used for more situations. Previously, different pools were used
  because query read timeouts were only ignored for certain DDL types and
  object types.

* When any command introspects multiple schemas on a database, the same
  connection pool is now reused. Previously, a separate pool (which selected
  the introspected schema as default database) was used for each. Instead,
  introspection SHOW queries now use schema name qualifiers before object
  names when appropriate.

* During integration testing, introspection connection pools are now fully
  maintained across multiple command handler invocations, and even across
  different tests. This improves test execution speed, while also massively
  cutting down on the TIME_WAIT pileup. This change was particularly essential
  for running multi-flavor integration tests on a fast machine, which
  previously could result in exhaustion of ephemeral ports if tests executed
  too quickly.

* Several additional code paths now reuse a connection pool with no default
  database selected; this is achieved by prefixing object names with the
  schema name.

* Schema introspection logic has been refactored substantially. This is mostly
  just an organizational change; future refactors are still needed to remove
  use of sqlx Select (struct-scanning) and reduce regex usage in fixup logic.

* During schema introspection, querying information_schema.views is now
  skipped if the earlier query to information_schema.tables already indicated
  that no views were present in the schema. (Skeema Premium only, but in the

internal/tengo/instance.go
@@ -178,6 +178,10 @@ func (instance *Instance) CachedConnectionPool(defaultSchema, params string) (*s
 	return db, err
 }
 
+func (instance *Instance) maxConnsPerPool() int {
+	return max(2, instance.maxUserConns-10)
+}
+
 func (instance *Instance) rawConnectionPool(defaultSchema, fullParams string, alreadyLocked bool) (*sqlx.DB, error) {
 	fullDSN := instance.BaseDSN + defaultSchema + "?" + fullParams
 	db, err := sqlx.Connect(instance.Driver, fullDSN)
@@ -192,7 +196,7 @@ func (instance *Instance) rawConnectionPool(defaultSchema, fullParams string, al
 	// the database side either globally or for this user. This does not completely
 	// eliminate max-conn problems, because each Instance can have many separate
 	// connection pools, but it may help.
-	db.SetMaxOpenConns(max(2, instance.maxUserConns-10))
+	db.SetMaxOpenConns(instance.maxConnsPerPool())
 
 	// Set max conn reuse lifetime to 1 minute, and set max idle time based on
 	// the session wait_timeout or 10s max.
@@ -214,9 +218,19 @@ func (instance *Instance) CanConnect() (bool, error) {
 	// half of TengoIntegrationSuite.TestInstanceCanConnect() in instance_test.go.
 	// That logic was commented out since this logic changes so rarely, and the
 	// test setup there is especially disruptive when using tmpfs containers.
+	if !instance.valid {
+		// We haven't ever successfully connected yet, so we can safely call Valid()
+		// without getting a false-positive from a previous cached conn pool; and if
+		// successful, we can then re-use this newly cached pool for other code paths
+		return instance.Valid()
+	}
+
+	// Use a new connection pool and intentionally avoid the pool cache
 	db, err := instance.ConnectionPool("", "")
 	if db != nil {
-		db.Close() // close immediately to avoid a buildup of sleeping idle conns
+		// close immediately since we bypassed cache and therefore the pool can't be
+		// reused elsewhere
+		db.Close()
 	}
 	return err == nil, err
 }
@@ -463,89 +477,8 @@ func (instance *Instance) SchemaNames() ([]string, error) {
 // more schema names as args to filter the result to just those schemas.
 // Note that the ordering of the resulting slice is not guaranteed.
 func (instance *Instance) Schemas(onlyNames ...string) ([]*Schema, error) {
-	db, err := instance.CachedConnectionPool("", "")
-	if err != nil {
-		return nil, err
-	}
-	var rawSchemas []struct {
-		Name      string `db:"schema_name"`
-		CharSet   string `db:"default_character_set_name"`
-		Collation string `db:"default_collation_name"`
-	}
-
-	var args []interface{}
-	var query string
-
-	// Note on these queries: MySQL 8.0 changes information_schema column names to
-	// come back from queries in all caps, so we need to explicitly use AS clauses
-	// in order to get them back as lowercase and have sqlx Select() work
-	if len(onlyNames) == 0 {
-		query = `
-			SELECT schema_name AS schema_name, default_character_set_name AS default_character_set_name,
-			       default_collation_name AS default_collation_name
-			FROM   information_schema.schemata
-			WHERE  schema_name NOT IN ('information_schema', 'performance_schema', 'mysql', 'test', 'sys')`
-	} else {
-		// If instance is using lower_case_table_names=2, apply an explicit collation
-		// to ensure the schema name comes back with its original lettercasing. See
-		// https://dev.mysql.com/doc/refman/8.0/en/charset-collation-information-schema.html
-		var lctn2Collation string
-		if instance.NameCaseMode() == NameCaseInsensitive {
-			lctn2Collation = " COLLATE utf8_general_ci"
-		}
-		query = fmt.Sprintf(`
-			SELECT schema_name AS schema_name, default_character_set_name AS default_character_set_name,
-			       default_collation_name AS default_collation_name
-			FROM   information_schema.schemata
-			WHERE  schema_name%s IN (?)`, lctn2Collation)
-		query, args, err = sqlx.In(query, onlyNames)
-	}
-	if err := db.Select(&rawSchemas, query, args...); err != nil {
-		return nil, err
-	}
-
-	schemas := make([]*Schema, len(rawSchemas))
-	for n, rawSchema := range rawSchemas {
-		schemas[n] = &Schema{
-			Name:      rawSchema.Name,
-			CharSet:   rawSchema.CharSet,
-			Collation: rawSchema.Collation,
-		}
-		// Create a non-cached connection pool with this schema as the default
-		// database. The instance.querySchemaX calls below can establish a lot of
-		// connections, so we will explicitly close the pool afterwards, to avoid
-		// keeping a very large number of conns open. (Although idle conns eventually
-		// get closed automatically, this may take too long.)
-		flavor := instance.Flavor()
-		schemaDB, err := instance.ConnectionPool(rawSchema.Name, instance.introspectionParams())
-		if err != nil {
-			return nil, err
-		}
-		if instance.maxUserConns >= 30 {
-			// Limit concurrency to 20, unless limit is already lower than this due to
-			// having a low maxUserConns (see logic in Instance.rawConnectionPool)
-			schemaDB.SetMaxOpenConns(20)
-
-			// Also increase max idle conns above the Golang default of 2, to ensure
-			// concurrent introspection queries reuse conns more effectively.
-			schemaDB.SetMaxIdleConns(20)
-		}
-		g, ctx := errgroup.WithContext(context.Background())
-		g.Go(func() (err error) {
-			schemas[n].Tables, err = querySchemaTables(ctx, schemaDB, rawSchema.Name, flavor)
-			return err
-		})
-		g.Go(func() (err error) {
-			schemas[n].Routines, err = querySchemaRoutines(ctx, schemaDB, rawSchema.Name, flavor, instance.NameCaseMode())
-			return err
-		})
-		err = g.Wait()
-		schemaDB.Close()
-		if err != nil {
-			return nil, err
-		}
-	}
-	return schemas, nil
+	opts := IntrospectionOptions{SchemaNames: onlyNames}
+	return IntrospectSchemas(context.Background(), instance, opts)
 }
 
 // SchemasByName returns a map of schema name string to *Schema.  If
@@ -599,16 +532,6 @@ func (instance *Instance) HasSchema(name string) (bool, error) {
 	}
 }
 
-// ShowCreateTable returns a string with a CREATE TABLE statement, representing
-// how the instance views the specified table as having been created.
-func (instance *Instance) ShowCreateTable(schema, table string) (string, error) {
-	db, err := instance.CachedConnectionPool(schema, instance.introspectionParams())
-	if err != nil {
-		return "", err
-	}
-	return showCreateTable(context.Background(), db, table)
-}
-
 // introspectionParams returns a params string which ensures safe session
 // variables for use with SHOW CREATE as well as queries on information_schema
 func (instance *Instance) introspectionParams() string {
@@ -646,16 +569,14 @@ func (instance *Instance) introspectionParams() string {
 	return v.Encode()
 }
 
-func showCreateTable(ctx context.Context, db *sqlx.DB, table string) (string, error) {
-	var row struct {
-		TableName       string `db:"Table"`
-		CreateStatement string `db:"Create Table"`
-	}
-	query := "SHOW CREATE TABLE " + EscapeIdentifier(table)
-	if err := db.GetContext(ctx, &row, query); err != nil {
+// ShowCreateTable returns a string with a CREATE TABLE statement, representing
+// how the instance views the specified table as having been created.
+func (instance *Instance) ShowCreateTable(schema, table string) (string, error) {
+	db, err := instance.CachedConnectionPool("", instance.introspectionParams())
+	if err != nil {
 		return "", err
 	}
-	return row.CreateStatement, nil
+	return showCreateTable(context.Background(), db, schema, table)
 }
 
 // TableSize returns an estimate of the table's size on-disk, based on data in
@@ -837,21 +758,7 @@ func (instance *Instance) DropSchema(schema string, opts BulkDropOptions) error 
 		// logic in DropTablesInSchema
 		err = dropSchema(db, schema)
 	}
-	if err != nil {
-		return err
-	}
-
-	// Close any connection pools for that database name
-	prefix := schema + "?"
-	instance.m.Lock()
-	defer instance.m.Unlock()
-	for key, connPool := range instance.connectionPool {
-		if strings.HasPrefix(key, prefix) {
-			connPool.Close()
-			delete(instance.connectionPool, key)
-		}
-	}
-	return nil
+	return err
 }
 
 // dropSchema executes a DROP DATABASE on the supplied database name. This isn't
@@ -914,7 +821,7 @@ func (opts BulkDropOptions) params() string {
 // DropTablesInSchema drops all tables in a schema. If opts.OnlyIfEmpty==true,
 // returns an error if any of the tables have any rows.
 func (instance *Instance) DropTablesInSchema(schema string, opts BulkDropOptions) error {
-	db, err := instance.CachedConnectionPool(schema, opts.params())
+	db, err := instance.CachedConnectionPool("", opts.params())
 	if err != nil {
 		return fmt.Errorf("Error obtaining connection pool for dropping tables in schema %s: %w", EscapeIdentifier(schema), err)
 	}
@@ -952,7 +859,7 @@ func (instance *Instance) DropTablesInSchema(schema string, opts BulkDropOptions
 					for n := range chunk {
 						escapedPartitionNames[n] = EscapeIdentifier(chunk[n])
 					}
-					alter := "ALTER TABLE " + EscapeIdentifier(name) + " DROP PARTITION " + strings.Join(escapedPartitionNames, ", ")
+					alter := "ALTER TABLE " + EscapeIdentifier(schema) + "." + EscapeIdentifier(name) + " DROP PARTITION " + strings.Join(escapedPartitionNames, ", ")
 					if _, err := db.Exec(alter); err != nil {
 						return fmt.Errorf("Error dropping partitions via %s: %w", alter, err)
 					}
@@ -971,7 +878,7 @@ func (instance *Instance) DropTablesInSchema(schema string, opts BulkDropOptions
 	for chunk := range slices.Chunk(names, max(opts.ChunkSize, 1)) {
 		escapedNames := make([]string, len(chunk))
 		for n := range chunk {
-			escapedNames[n] = EscapeIdentifier(chunk[n])
+			escapedNames[n] = EscapeIdentifier(schema) + "." + EscapeIdentifier(chunk[n])
 		}
 		_, err := db.Exec("DROP TABLE " + strings.Join(escapedNames, ", "))
 		if err != nil {
@@ -999,12 +906,12 @@ func (instance *Instance) DropRoutinesInSchema(schema string, opts BulkDropOptio
 		// DDLs per round-trip
 		params += "&multiStatements=true"
 	}
-	db, err := instance.CachedConnectionPool(schema, params)
+	db, err := instance.CachedConnectionPool("", params)
 	if err != nil && chunkSize > 1 {
 		// Try again without chunking, in case there's a proxy which doesn't support
 		// multiStatements or is configured to forbid it
 		chunkSize = 1
-		db, err = instance.CachedConnectionPool(schema, opts.params())
+		db, err = instance.CachedConnectionPool("", opts.params())
 	}
 	if err != nil {
 		return err
@@ -1052,7 +959,7 @@ func (instance *Instance) DropRoutinesInSchema(schema string, opts BulkDropOptio
 	for chunk := range slices.Chunk(routineInfo, chunkSize) {
 		chunkStrings := make([]string, 0, len(chunk))
 		for _, ri := range chunk {
-			chunkStrings = append(chunkStrings, "DROP "+ri.Type+" IF EXISTS "+EscapeIdentifier(ri.Name))
+			chunkStrings = append(chunkStrings, "DROP "+ri.Type+" IF EXISTS "+EscapeIdentifier(schema)+"."+EscapeIdentifier(ri.Name))
 		}
 		if chunkSize > 1 {
 			query := strings.Join(chunkStrings, ";") + ";"

internal/applier/ddlstatement.go
@@ -201,22 +201,17 @@ func getWrapper(config *mybase.Config, diff tengo.ObjectDiff, tableSize int64, m
 // getConnectParams returns the necessary connection params (session variables)
 // for the supplied diff and config.
 func getConnectParams(diff tengo.ObjectDiff, config *mybase.Config) string {
-	// Use unlimited query timeout for ALTER TABLE or DROP TABLE, since these
-	// operations can be slow on large tables.
+	// Use unlimited query timeout for all DDL statements, since ALTER TABLE or
+	// DROP TABLE can be slow on large tables. (Even though all other DDL is fast,
+	// skipping timeouts there too ensures we can re-use connection pools.)
 	// For ALTER TABLE, if requested, also use foreign_key_checks=1 if adding
 	// new foreign key constraints.
-	if td, ok := diff.(*tengo.TableDiff); ok && td.Type == tengo.DiffTypeAlter {
-		if config.GetBool("foreign-key-checks") {
-			_, addFKs := td.SplitAddForeignKeys()
-			if addFKs != nil {
-				return "readTimeout=0&foreign_key_checks=1"
-			}
+	if diff.DiffType() == tengo.DiffTypeAlter && diff.ObjectKey().Type == tengo.ObjectTypeTable && config.GetBool("foreign-key-checks") {
+		if _, addFKs := diff.(*tengo.TableDiff).SplitAddForeignKeys(); addFKs != nil {
+			return "readTimeout=0&foreign_key_checks=1"
 		}
-		return "readTimeout=0"
-	} else if ok && td.Type == tengo.DiffTypeDrop {
-		return "readTimeout=0"
 	}
-	return ""
+	return "readTimeout=0"
 }
 
 // Execute runs the DDL statement, either by running a SQL query against a DB,

internal/tengo/errors_test.go
@@ -11,7 +11,7 @@ func (s TengoIntegrationSuite) TestIsDatabaseError(t *testing.T) {
 	if IsDatabaseError(err1) {
 		t.Errorf("IsDatabaseError unexpectedly returned true for non-database error type=%T", err1)
 	}
-	_, err2 := s.d.CachedConnectionPool("doesnt_exist", "")
+	_, err2 := s.d.ConnectionPool("doesnt_exist", "")
 	if !IsDatabaseError(err2) {
 		t.Errorf("IsDatabaseError unexpectedly returned false for error of type=%T", err2)
 	}
@@ -23,17 +23,17 @@ func (s TengoIntegrationSuite) TestDatabaseErrorTypeFunctions(t *testing.T) {
 	if IsSyntaxError(err) {
 		t.Errorf("IsSyntaxError unexpectedly returned true for non-database error type=%T", err)
 	}
-	db, err := s.d.ConnectionPool("testing", "")
+	db, err := s.d.CachedConnectionPool("", "")
 	if err != nil {
 		t.Fatalf("Unable to get connection")
 	}
-	_, syntaxErr := db.Exec("ALTER TAABBEL actor ENGINE=InnoDB")
+	_, syntaxErr := db.Exec("ALTER TAABBEL testing.actor ENGINE=InnoDB")
 	if syntaxErr == nil {
 		t.Error("Bad syntax still returned nil error unexpectedly")
 	} else if !IsSyntaxError(syntaxErr) {
 		t.Errorf("Error of type %T %+v unexpectedly not considered syntax error", syntaxErr, syntaxErr)
 	}
-	_, doesntExistErr := db.Exec("ALTER TABLE doesnt_exist ENGINE=InnoDB")
+	_, doesntExistErr := db.Exec("ALTER TABLE testing.doesnt_exist ENGINE=InnoDB")
 	if doesntExistErr == nil {
 		t.Error("Bad alter still returned nil error unexpectedly")
 	} else if IsSyntaxError(doesntExistErr) {
@@ -49,11 +49,11 @@ func (s TengoIntegrationSuite) TestDatabaseErrorTypeFunctions(t *testing.T) {
 	}
 
 	// Test IsSessionVarNameError and IsSessionVarValueError
-	_, invalidVarNameErr := s.d.ConnectionPool("testing", "invalidvar='hello'")
+	_, invalidVarNameErr := s.d.ConnectionPool("", "invalidvar='hello'")
 	_, globalOnlyVarNameErr := s.d.ConnectionPool("", "concurrent_insert=1")
 	_, readOnlyVarNameErr := s.d.ConnectionPool("", "version_comment='hello'")
-	_, invalidVarValueErr := s.d.ConnectionPool("testing", "sql_mode='superduperdb'")
-	_, invalidVarValTypeErr := s.d.ConnectionPool("testing", "wait_timeout='hello'")
+	_, invalidVarValueErr := s.d.ConnectionPool("", "sql_mode='superduperdb'")
+	_, invalidVarValTypeErr := s.d.ConnectionPool("", "wait_timeout='hello'")
 	if !IsSessionVarNameError(invalidVarNameErr) {
 		t.Errorf("Incorrect behavior of IsSessionVarNameError: expected true for %T %+v, but false was returned", invalidVarNameErr, invalidVarNameErr)
 	}

internal/tengo/instance_test.go
@@ -151,13 +151,11 @@ func TestInstanceIntrospectionParams(t *testing.T) {
 	assertParams("mysql:8.0", "NO_FIELD_OPTIONS,NO_BACKSLASH_ESCAPES,NO_KEY_OPTIONS,NO_TABLE_OPTIONS", "sql_quote_show_create=1&collation=binary&information_schema_stats_expiry=0&sql_mode=%27NO_BACKSLASH_ESCAPES%27")
 }
 
-func (s TengoIntegrationSuite) TestInstanceConnect(t *testing.T) {
+func (s TengoIntegrationSuite) TestInstanceCachedConnectionPool(t *testing.T) {
 	// Connecting to invalid schema should return an error
-	db, err := s.d.CachedConnectionPool("does-not-exist", "")
-	if err == nil {
-		t.Error("err is unexpectedly nil")
-	} else if db != nil {
-		t.Error("db is unexpectedly non-nil")
+	db, err := s.d.CachedConnectionPool("doesnotexist", "")
+	if err == nil || db != nil {
+		t.Error("unexpected return values from CachedConnectionPool")
 	}
 
 	// Connecting without specifying a default schema should be successful
@@ -190,6 +188,23 @@ func (s TengoIntegrationSuite) TestInstanceConnect(t *testing.T) {
 		t.Errorf("Unexpected connection error: %s", err)
 	} else if db4 == db || db4 == db3 {
 		t.Error("Expected different DB pool to be returned from Connect with different params; instead was same")
+	}
+
+	// Check cache directly, and close two of these pools which won't be reused by
+	// other tests
+	key3 := "information_schema?" + s.d.BuildParamString("")
+	if dbByKey3, ok := s.d.Instance.connectionPool[key3]; ok && db3 == dbByKey3 {
+		delete(s.d.Instance.connectionPool, key3)
+		db3.Close()
+	} else {
+		t.Errorf("db3 not found in cache with expected key %q", key3)
+	}
+	key4 := "information_schema?" + s.d.BuildParamString("foreign_key_checks=0&wait_timeout=20")
+	if dbByKey4, ok := s.d.Instance.connectionPool[key4]; ok && db4 == dbByKey4 {
+		delete(s.d.Instance.connectionPool, key4)
+		db4.Close()
+	} else {
+		t.Errorf("db4 not found in cache with expected key %q", key3)
 	}
 }
 
@@ -351,8 +366,10 @@ func (s TengoIntegrationSuite) TestInstanceGrantChecks(t *testing.T) {
 	if !s.d.CanSkipBinlog() {
 		t.Fatal("Expected all Dockerized instances to be able to skip binlogs, but CanSkipBinlogs returned false")
 	}
-	if _, err := s.d.CachedConnectionPool("", "sql_log_bin=0"); err != nil {
+	if db, err := s.d.ConnectionPool("", "sql_log_bin=0"); err != nil {
 		t.Errorf("Error connecting with sql_log_bin=0: %v", err)
+	} else {
+		db.Close()
 	}
 
 	// Now create a less-privileged user, and confirm behavior. Note that for
@@ -736,11 +753,6 @@ func (s TengoIntegrationSuite) TestInstanceDropRoutinesInSchema(t *testing.T) {
 	// Repeated calls should have no effect, no error.
 	if err := s.d.DropRoutinesInSchema("testing", opts); err != nil {
 		t.Errorf("Unexpected error from DropRoutinesInSchema: %v", err)
-	}
-
-	// Calling on a nonexistent schema name should return an error.
-	if err := s.d.DropRoutinesInSchema("doesntexist", opts); err == nil {
-		t.Error("Expected error from DropRoutinesInSchema on nonexistent schema; instead err was nil")
 	}
 }
 

commit 729c8055a06035481ccb39e882acb6ab5ebae004

docker: fix recent race condition on slow machines
Last month's connection-reuse refactor 49b79d1 inadvertently introduced a
race condition when interacting with a database server that is being
initialized for the first time. In this situation, a connection can
succeed but then get dropped immediately as the server restarts during its
initialization process; the refactored code could then cache a bad conn.

In practical terms, this only affects Skeema-managed Docker instances, as that
is the only realistic situation in which a connection is launched against a
server that is being newly initialized. Furthermore the race condition only
seems to occur on slower machines, and possibly only with older flavor images.
In any case, this commit catches this scenario, so the "is the Docker
container reachable yet" code path won't return a false positive.

This commit also has the side-effect of making the server variable
introspection logic mandatory, whereas previously failures in that query were
intentionally ignored. This is not expected to have any user-facing impact, as
there are no known supported server flavors where the queried global variables
do not exist.

internal/tengo/instance.go
@@ -189,7 +189,10 @@ func (instance *Instance) rawConnectionPool(defaultSchema, fullParams string, al
 		return nil, err
 	}
 	if !instance.valid {
-		instance.hydrateVars(db, !alreadyLocked)
+		err = instance.hydrateVars(db, !alreadyLocked)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// Set max concurrent connections, ensuring it is less than any limit set on
@@ -355,15 +358,13 @@ func (instance *Instance) AdaptiveHashIndexEnabled() bool {
 }
 
 // hydrateVars populates several non-exported Instance fields by querying
-// various global and session variables. Failures are ignored; these variables
-// are designed to help inform behavior but are not strictly mandatory.
-func (instance *Instance) hydrateVars(db *sqlx.DB, lock bool) {
-	var err error
+// various global and session variables.
+func (instance *Instance) hydrateVars(db *sqlx.DB, lock bool) (err error) {
 	if lock {
 		instance.m.Lock()
 		defer instance.m.Unlock()
 		if instance.valid {
-			return
+			return nil
 		}
 	}
 
@@ -380,7 +381,7 @@ func (instance *Instance) hydrateVars(db *sqlx.DB, lock bool) {
 	// number.
 	conn, err := db.Conn(ctx)
 	if err != nil {
-		return
+		return err
 	}
 	defer conn.Close()
 	start := time.Now()
@@ -393,7 +394,7 @@ func (instance *Instance) hydrateVars(db *sqlx.DB, lock bool) {
 		&maxUserConns, &maxConns,
 		&instance.lowerCaseNames, &instance.ahiEnabled)
 	if err != nil {
-		return
+		return err
 	}
 	instance.valid = true
 	if instance.flavor == FlavorUnknown { // Only set flavor if it wasn't already forced to some value
@@ -405,6 +406,7 @@ func (instance *Instance) hydrateVars(db *sqlx.DB, lock bool) {
 	} else {
 		instance.maxUserConns = maxConns
 	}
+	return nil
 }
 
 // Regular expression defining privileges that allow use of setting session